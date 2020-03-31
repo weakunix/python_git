1adc4e40a5fad36cf0fe8c17dcebb966ac600e65
R. Xu <ruoyu.xu2008@icloud.com>
2020-04-01 01:36:19 +0800

    Euler 18 incomplete

--- euler18.py
+++ euler18.py
@@ -1,7 +1,12 @@
+#lists
 rows = []
+
+#read number triangle txt file
 with open('data_e18.txt','r') as fp:
     for line in fp:
         row = line.split()
         row = [int(r) for r in row]
         rows.append(row)
+
+#try every route
 
