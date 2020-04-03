5fd6dfcac7f6f1f495c8f49a99c08e6d749b5c17
R. Xu <ruoyu.xu2008@icloud.com>
2020-04-03 08:53:52 -0400

    Hello World

--- hello.py
+++ hello.py
@@ -1,1 +1,6 @@
-print("Hello World!")
+import sys
+
+#fp = open('test.txt','w')
+#print("Hello World! (stderr)",file=fp)
+print("Hello World! (stderr)",file=sys.stderr)
+print("Hello World! (stdout)",file=sys.stdout)
