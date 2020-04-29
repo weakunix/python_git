b4907afc7a1d71aa5c4b50a5a5ccc85140455164
R. Xu <ruoyu.xu2008@icloud.com>
2020-04-29 10:03:55 +0800

    resolving log and sD and commit of incomplete euler 18

--- project_euler/euler18.py
+++ project_euler/euler18.py
@@ -1,10 +1,11 @@
 #vars
 maxsum = 0
-numsum = 0
 count = 0
 
 #lists
 rows = []
+numsum = [75]
+l = []
 
 #read number triangle txt file
 with open('data_e18.txt','r') as fp:
@@ -14,3 +15,28 @@ with open('data_e18.txt','r') as fp:
         rows.append(row)
 
 #try every route
+for a in range(0, 2):
+    for b in range(0, 2):
+        for c in range(0, 2):
+            for d in range(0, 2):
+                for e in range(0, 2):
+                    for f in range(0, 2):
+                        for g in range(0, 2):
+                            for h in range(0, 2):
+                                for i in range(0, 2):
+                                    for j in range(0, 2):
+                                        for k in range(0, 2):
+                                            for l in range(0, 2):
+                                                for m in range(0, 2):
+                                                    for n in range(0, 2):
+                                                        count = 0
+                                                        numsum = [75]
+                                                        l = [a, b, c, d, e, f, g, h, i, j, k, l, m, n]
+                                                        for v in range(14):
+                                                            count += l[v]
+                                                            v += 1
+                                                            numsum.append(rows[v][count])
+                                                        numsum = sum(numsum)
+                                                        if numsum > maxsum:
+                                                            maxsum = numsum
+print(maxsum)
