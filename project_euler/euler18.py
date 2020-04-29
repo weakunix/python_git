4a6987dc9127ebe5d27331c79f91e0e88df372df
R. Xu <ruoyu.xu2008@icloud.com>
2020-04-29 22:29:20 +0800

    Euler 18 complete!!!!

--- project_euler/euler18.py
+++ project_euler/euler18.py
@@ -1,13 +1,10 @@
 #vars
 maxsum = 0
 count = 0
-
-#lists
 rows = []
 numsum = [75]
-l = []
 
-#read number triangle txt file
+#reading data_e18.txt
 with open('data_e18.txt','r') as fp:
     for line in fp:
         row = line.split()
@@ -15,28 +12,66 @@ with open('data_e18.txt','r') as fp:
         rows.append(row)
 
 #try every route
-for a in range(0, 2):
-    for b in range(0, 2):
-        for c in range(0, 2):
-            for d in range(0, 2):
-                for e in range(0, 2):
-                    for f in range(0, 2):
-                        for g in range(0, 2):
-                            for h in range(0, 2):
-                                for i in range(0, 2):
-                                    for j in range(0, 2):
-                                        for k in range(0, 2):
-                                            for l in range(0, 2):
-                                                for m in range(0, 2):
-                                                    for n in range(0, 2):
-                                                        count = 0
-                                                        numsum = [75]
-                                                        l = [a, b, c, d, e, f, g, h, i, j, k, l, m, n]
-                                                        for v in range(14):
-                                                            count += l[v]
-                                                            v += 1
-                                                            numsum.append(rows[v][count])
-                                                        numsum = sum(numsum)
-                                                        if numsum > maxsum:
-                                                            maxsum = numsum
+for i in range(1, 2 ** 14 + 1):
+    numsum = [75]
+    count = 0
+    if i > 2 ** 13:
+        count += 1
+        i -= 2 ** 13
+    numsum.append(rows[1][count])
+    if i > 2 ** 12:
+        count += 1
+        i -= 2 ** 12
+    numsum.append(rows[2][count])
+    if i > 2 ** 11:
+        count += 1
+        i -= 2 ** 11
+    numsum.append(rows[3][count])
+    if i > 2 ** 10:
+        count += 1
+        i -= 2 ** 10
+    numsum.append(rows[4][count])
+    if i > 2 ** 9:
+        count += 1
+        i -= 2 ** 9
+    numsum.append(rows[5][count])
+    if i > 2 ** 8:
+        count += 1
+        i -= 2 ** 8
+    numsum.append(rows[6][count])
+    if i > 2 ** 7:
+        count += 1
+        i -= 2 ** 7
+    numsum.append(rows[7][count])
+    if i > 2 ** 6:
+        count += 1
+        i -= 2 ** 6
+    numsum.append(rows[8][count])
+    if i > 2 ** 5:
+        count += 1
+        i -= 2 ** 5
+    numsum.append(rows[9][count])
+    if i > 2 ** 4:
+        count += 1
+        i -= 2 ** 4
+    numsum.append(rows[10][count])
+    if i > 2 ** 3:
+        count += 1
+        i -= 2 ** 3
+    numsum.append(rows[11][count])
+    if i > 2 ** 2:
+        count += 1
+        i -= 2 ** 2
+    numsum.append(rows[12][count])
+    if i > 2:
+        count += 1
+        i -= 2
+    numsum.append(rows[13][count])
+    if i > 1:
+        count += 1
+    numsum.append(rows[14][count])
+    numsum = sum(numsum)
+    if numsum > maxsum:
+        maxsum = numsum
 print(maxsum)
+
