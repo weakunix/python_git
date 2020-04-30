f73f02abc3dc0ebfd1d255c27d43c988a616d5b2
Cowland <kingofcowlandmrmoooo@gmail.com>
2020-04-30 17:32:00 -0400

    Big push! contains foundation for new game and everything

--- coconut2d/TheAnnoyingPhoneAdGame/game.py
+++ coconut2d/TheAnnoyingPhoneAdGame/game.py
@@ -1,4 +1,82 @@
+# Imports
 import cocos
 import pyglet
+# Files
 import game_menu
 import game_sprites
+
+# end of imports
+# g-Vars
+sceneNumber = 0
+
+
+# Essentials
+class showScore(cocos.layer.Layer):
+    def __init__(self):
+        super().__init__()
+        label = cocos.text.Label(
+            "Score:",
+            font_name="Times New Roman",
+            font_size=20,
+            anchor_x="center",
+            anchor_y="center"
+        )
+        size = cocos.director.director.get_window_size()
+        print(size)
+        label.position = size[0] / 15, size[1] / 1.1
+        self.add(label)
+
+
+# background
+class Background(cocos.layer.Layer):
+    def __init__(self):
+        super().__init__()
+        img = cocos.sprite.Sprite("background.jpg")
+        img.scale = 1
+        img.position = img.width // 2, img.height // 2
+
+        self.px_width = 1366
+        self.px_height = 768
+
+        self.add(img)
+
+
+# start
+if __name__ == "__main__":
+    cocos.director.director.init(width=1280, height=720, caption="The Annoying Phone Game")
+
+    # cursor
+    cursor = game_sprites.makeCursor()
+    cocos.director.director.window.set_mouse_cursor(cursor)
+    # background
+    bg = Background()
+    # score
+    sS = showScore()
+    # title screen
+    ttl = game_menu.printTitle()
+    btn1 = game_menu.buttonStart()
+    # sprites
+
+    # scenes
+    # menu
+    menuScene = cocos.scene.Scene()
+    menuScene.add(bg)
+    menuScene.add(ttl)
+    menuScene.add(btn1)
+    # game
+    gameScene = cocos.scene.Scene()
+    gameScene.add(bg)
+    gameScene.add(sS)
+    # run
+    # menu scene
+    if sceneNumber == 0:
+        cocos.director.director.run(menuScene)
+    if sceneNumber == 1:
+        cocos.director.director.pop(menuScene)
+        cocos.director.director.replace(gameScene)
+        cocos.director.director.push(gameScene)
+
+
+def start():
+    global sceneNumber
+    sceneNumber = 1
--- coconut2d/TheAnnoyingPhoneAdGame/game_menu.py
+++ coconut2d/TheAnnoyingPhoneAdGame/game_menu.py
@@ -1,3 +1,48 @@
 import cocos
 import pyglet
+import game
 
+
+class printTitle(cocos.layer.Layer):
+    def __init__(self):
+        super().__init__()
+        label = cocos.text.Label(
+            "The Annoying Phone Game",
+            font_name="Comic Sans",
+            font_size=20,
+            anchor_x="center",
+            anchor_y="center"
+        )
+        size = cocos.director.director.get_window_size()
+        label.position = size[0] / 2, size[1] / 1.5
+        self.add(label)
+
+
+class buttonStart(cocos.layer.Layer):
+    is_event_handler = True
+
+    def __init__(self):
+        super().__init__()
+        btn = pyglet.image.load("btnsheet.png")
+        btn_sliced = pyglet.image.ImageGrid(btn, 1, 2, item_width=102, item_height=51)
+        anim = pyglet.image.Animation.from_image_sequence(btn_sliced[0:], 1, loop=True)
+        self.spr = cocos.sprite.Sprite(anim, anchor=(0, 0))
+        size = cocos.director.director.get_window_size()
+        self.spr.position = (size[0] / 2)-self.spr.width, size[1] / 3
+        self.spr.scale = 2
+        self.add(self.spr)
+        self.clicked = False
+
+    def MouseOnSprite(self, x, y):
+        if self.spr.x + self.spr.width > x > self.spr.x:
+            if self.spr.y + self.spr.height > y > self.spr.y:
+                print(1)
+                return True
+        return False
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        if button & pyglet.window.mouse.LEFT:
+            if self.MouseOnSprite(x, y):
+                self.clicked = True
+                print("game starts")
+                game.start()
