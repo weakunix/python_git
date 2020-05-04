d4e7d1f7319ac053550464c4622c620b29701f26
Cowland <kingofcowlandmrmoooo@gmail.com>
2020-05-03 22:02:57 -0400

    With new algorithem. Need (ruo)yu to make improvements! (sry for the bad pun)

--- live_events/coconut2d/badiphonegameport/other stuff/project 684/project684.py
+++ live_events/coconut2d/badiphonegameport/other stuff/project 684/project684.py
@@ -1,4 +1,9 @@
 import random
-len = int(input("length and width (1 number)\n")) #length and width
-gameArray = [[random.randint(0, 3) for x in range(0, len)]for y in range(0, len)]
+
+leng = int(input("length and width (1 number)\n"))  # length and width
+gameArray = [[random.randint(0, 3) for x in range(0, leng)] for y in range(0, leng)]
 print(gameArray)
+# to read
+for i in range(len(gameArray)):
+    for ii in range(len(gameArray[i])):
+        print(gameArray[i][ii])
