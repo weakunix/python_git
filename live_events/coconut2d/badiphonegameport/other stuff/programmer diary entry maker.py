e898646c129739bd68cb3dbffba3c331bf2774c9
CowlandonIpad <kingofcowlandmrmoooo@gmail.com>
2020-05-03 22:39:19 -0400

    dev diary update #1
    Got all the stuff needed for DDiary. RUOYU THANK YOU SO MUCH FOR
    PJ495!!!!!

---
+++ live_events/coconut2d/badiphonegameport/other stuff/programmer diary entry maker.py
@@ -0,0 +1,20 @@
+import datetime
+import os
+import socket
+lines = []
+with open('counter.txt', 'r') as b:
+    for line in b:
+        lines.append(line[:-1])
+print(lines)
+print("starting to make entry")
+title = input("title of your marvolous art work?\n >>>")
+desc = input("description of the mona lisa? (make this a breif TLDR; a summary)")
+h = open("devDiary.txt", "a")
+m = int(lines[0])
+m+=1
+s = open("counter.txt", "w")
+s.write(str(m))
+s.close()
+h.write("\n=====================\nJournal Entry #"+str(m)+"\nAt: "+str(datetime.datetime.now())+"\nTitle: "+title+"\n Description:"+desc+"\nAuthor: "+str(socket.gethostname())+"\n====================\nContents:\n")
+h.close()
+print("entry done")
