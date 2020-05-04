b7096a3066863687d27f68520745a0c3ab9f157d
Cowland <kingofcowlandmrmoooo@gmail.com>
2020-05-04 10:08:32 -0400

    pushing from pycharm with ruoyu's "pull request"!

--- live_events/coconut2d/badiphonegameport/other stuff/programmer diary entry maker.py
+++ live_events/coconut2d/badiphonegameport/other stuff/programmer diary entry maker.py
@@ -2,16 +2,15 @@ import datetime
 import os
 import socket
 lines = []
-with open('counter.txt', 'r') as b:
+with open("counter.txt", 'r') as b:
     for line in b:
-        lines.append(line[:-1])
-print(lines)
+        lines.append(line[0:])
 print("starting to make entry")
 title = input("title of your marvolous art work?\n >>>")
 desc = input("description of the mona lisa? (make this a breif TLDR; a summary)")
-m = int(lines[0])
-m+=1
+m = int(str(lines[0]))
 s = open("counter.txt", "w")
+m+=1
 s.write(str(m))
 s.close()
 h = open("devDiary.txt", "a")
