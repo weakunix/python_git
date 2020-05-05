cf3df28038a7aae950769dd99af4483f97460d0c
R. Xu <ruoyu.xu2008@icloud.com>
2020-05-05 14:16:32 -0400

    Euler 35

--- live_events/project_euler/euler35.py
+++ live_events/project_euler/euler35.py
@@ -2,45 +2,30 @@
 import numpy as np
 
 #vars
-count = 0
-alln = []
+prime = set()
+nonp = set()
 
 #user def functions
-#shift 
-def shift_num(x):
-    x = str(x)
-    x = x[1:] + x[0] 
-    return int(x)
-
 #test for prime
 def is_prime(x):
-    for i in range(2, int(np.sqrt(x))):
+    for i in range(2, int(np.sqrt(x) + 1)):
         if x % i == 0:
             return False
     return True
 
 #trying each number
 for i in range(2, 1000000): #change to 1 million later
-    num = 0
-    i = str(i)
-    if '2' in i or '4' in i or '6' in i or '8' in i or '0' in i:
-        i = int(i)
-        if i != 2:
-            i = False
-    elif '5' in i and i != '5':
-        i = False
+    p = is_prime(i)
+    print(i)
+    if p:
+        prime.add(i)
     else:
-        i = int(i)
-        if i % 3 == 0 and i != 3:
-            i = False
-    if i:
-        for k in range(len(str(i)) - 1):
-            k = is_prime(i)
-            if not k:
-                i = False
-                break
-            i = shift_num(i)
-        if i:
-            alln.append(i)
-            count += 1
-print(count, '\n', alln)
+        nonp.add(i)
+for i in nonp:
+    print(i)
+    num = i
+    for k in range(len(str(i)) - 1):
+        num = int(str(num)[1:] + str(num)[0])
+        if num in prime and len(str(num)) == len(str(i)):
+            prime.remove(num)
+print(len(prime), '\n', prime)
