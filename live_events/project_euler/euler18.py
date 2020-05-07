0a7b3aac232f6f60bbf431919df2845b75f2255a
R. Xu <ruoyu.xu2008@icloud.com>
2020-05-07 13:18:59 -0400

    Euler 18 with efficient strategy and Euler 67

--- live_events/project_euler/euler18.py
+++ live_events/project_euler/euler18.py
@@ -1,26 +1,24 @@
 #vars
-maxsum = 0
 rows = []
-numsum = [75]
-column = 0
+row = []
 
 #reading data_e18.txt
-with open('data_e18.txt','r') as fp:
-   for line in fp:
-      row = line.split()
-      row = [int(r) for r in row]
-      rows.append(row)
+with open('data_e18.txt', 'r') as fp:
+    for line in fp:
+        row = line.split()
+        row = [int(r) for r in row]
+        rows.append(row)
 
-#trying all routes
-for i in range(2 ** 14):
-    numsum = [75]
-    column = 0
-    for count in range(1, 15):
-        if i & 1 == 1:
-            column += 1
-        numsum.append(rows[count][column])
-        i >>= 1
-    numsum = sum(numsum)
-    if numsum > maxsum:
-        maxsum = numsum
-print(maxsum)
+#finding largest route
+for i in range(-13, 1):
+    row = []
+    i = -i
+    for k in range(i + 1):
+        if rows[-1][k] > rows[-1][k + 1]:
+            row.append(rows[-1][k] + rows[-2][k])
+        else:
+            row.append(rows[-1][k + 1] + rows[-2][k])
+    rows.pop()
+    rows.pop()
+    rows.append(row)
+print(rows[0][0])
--- live_events/project_euler/euler67.py
+++ live_events/project_euler/euler67.py
@@ -1,32 +1,24 @@
-import datetime as dt
-
 #vars
-maxsum = 0
 rows = []
-numsum = [75]
-column = 0
+row = []
 
 #reading data_e18.txt
-with open('p067_triangle.txt','r') as fp:
-   for line in fp:
-      row = line.split()
-      row = [int(r) for r in row]
-      rows.append(row)
+with open('p067_triangle.txt', 'r') as fp:
+    for line in fp:
+        row = line.split()
+        row = [int(r) for r in row]
+        rows.append(row)
 
-#trying all routes
-t1 = dt.datetime.now()
-for i in range(2 ** 99):
-    numsum = [59]
-    if i % 1000 == 0:
-        passed = (dt.datetime.now() - t1).total_seconds()
-        print('{}: max={} elapsed={}s'.format(i,maxsum,passed))
-    column = 0
-    for count in range(1, 100):
-        if i & 1 == 1:
-            column += 1
-        numsum.append(rows[count][column])
-        i >>= 1
-    numsum = sum(numsum)
-    if numsum > maxsum:
-        maxsum = numsum
-print(maxsum)
+#finding largest route
+for i in range(-98, 1):
+    row = []
+    i = -i
+    for k in range(i + 1):
+        if rows[-1][k] > rows[-1][k + 1]:
+            row.append(rows[-1][k] + rows[-2][k])
+        else:
+            row.append(rows[-1][k + 1] + rows[-2][k])
+    rows.pop()
+    rows.pop()
+    rows.append(row)
+print(rows[0][0])
