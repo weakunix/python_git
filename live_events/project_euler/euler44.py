1114c188ffdd74e585561ebef0f8c2b72482f183
R. Xu <ruoyu.xu2008@icloud.com>
2020-06-02 17:59:54 -0400

    Euler 44 not complete

---
+++ live_events/project_euler/euler44.py
@@ -0,0 +1,13 @@
+#vars
+leastd = 0
+count = 0
+n1 = 0
+n2 = 0
+
+#user def functions
+#get pentagonal numbers
+def pentagonal(num):
+    return num * (3 * num - 1) / 2
+
+#testing for pentagonal sum and difference
+
