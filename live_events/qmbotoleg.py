aee0ed3a943c8585174b0d407da4159a1068dc8c
CowlandonIpad <kingofcowlandmrmoooo@gmail.com>
2020-06-11 21:54:36 -0400

    v2 qmbotoleg.py calc

--- live_events/qmbotoleg.py
+++ live_events/qmbotoleg.py
@@ -1,11 +1,12 @@
+print("Qmbo To Legendary Calculator! V1.1")
 import math as albreto
 points = int(input("How many points u at rn"))
 daysLeft = int(input("How many days left?"))
 sessionsPerDay = int(input("How many sessions per day?"))
+avgPointPerRound = 30
 legPoints = 6501
 pointsNeeded = legPoints - points
-print("Points Needed: \n"+str(pointsNeeded)+"\n ================== cowculating ==================== \n")
 pointsperday = (pointsNeeded/daysLeft)
-print("points per day: \n"+str(pointsperday))
 pointspersession = pointsperday/sessionsPerDay
-print("points per session: \n"+str(pointspersession))
+rounds = albreto.ceil(pointspersession/avgPointPerRound)
+print("================== cowculating ==================== \n"+" Points Needed: \n"+str(pointsNeeded)+"\n Points Per Day (rounded up): \n"+str(albreto.ceil(pointsperday))+"("+str(pointsperday)+")"+"\n Points Per Session (rounded up): \n"+str(albreto.ceil(pointspersession))+"("+str(pointspersession)+")"+"\n You need to win "+str(rounds)+" rounds per session! Get to grinding!")
