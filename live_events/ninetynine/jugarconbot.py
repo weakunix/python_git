1086e1ffefd801a76d5c7937c2831570a15b8bf9
Ruoyu Xu <ruoyu.xu2008@icloud.com>
2020-06-15 10:54:44 -0400

    99 with bot complete

--- live_events/ninetynine/jugarconbot.py
+++ live_events/ninetynine/jugarconbot.py
@@ -8,7 +8,7 @@ cardn = 0 #amount of cards per player
 bcard = [] #bot cards
 pcard = [] #player cards
 inpt = '' #input
-nums = ['2', '3', '4', '5', '6', '7', '8', '9', '10'] #number cards
+nums = ['2', '3', '4', '5', '6', '7', '8', '9', '10'] #number cards	
 
 #pre game set ups
 ##filling cardl
@@ -43,8 +43,8 @@ for i in range(cardn): #bot
 	bcard.append(cardl.pop(random.randint(0, len(cardl) - 1)))
 	
 #user def functions
-##replace card
-def replace_card(c):
+##player replace card
+def p_replace_card(c):
 	global pcard
 	count = -1
 	for i in pcard:
@@ -52,6 +52,19 @@ def replace_card(c):
 		if i == c:
 			pcard.pop(count)
 			pcard.append(cardl.pop(random.randint(0, len(cardl) - 1)))
+			break
+
+##bot replace card
+def b_replace_card(c):
+	global bcard
+	count = -1
+	for i in bcard:
+		count += 1
+		if i == c:
+			bcard.pop(count)
+			bcard.append(cardl.pop(random.randint(0, len(cardl) - 1)))
+			break
+			
 ##player plays
 def player():
 	global pcard
@@ -82,7 +95,7 @@ def player():
 								sumc += 1
 							else:
 								sumc += 11
-							replace_card(1)
+							p_replace_card(1)
 							break
 						else:
 							print('\nInput was not 1 or 11\n')
@@ -92,41 +105,134 @@ def player():
 					if inpt[1] == 'a' or inpt[1] == 'A': #jack
 						if 11 in pcard:
 							sumc += 10
-							replace_card(11)
+							p_replace_card(11)
 							break
 					elif inpt[1] == 'o' or inpt[1] == 'O': #joker
 						if 14 in pcard:
 							sumc = 99
-							replace_card(14)
+							p_replace_card(14)
 							break
 			elif inpt[0] == 'q' or inpt[0] == 'Q': #queen
 				if 12 in pcard:
 					sumc += 10
-					replace_card(12)
+					p_replace_card(12)
 					break
 			elif inpt[0] == 'k' or inpt[0] == 'K': #king
 				if 13 in pcard:
 					sumc = 99
-					replace_card(13)
+					p_replace_card(13)
 					break
 			elif inpt in nums:
 				inpt = int(inpt)
 				if inpt == 4: #4
-					replace_card(4)
+					p_replace_card(4)
 					break
 				elif inpt == 9: #9
-					replace_card(9)
+					p_replace_card(9)
+					break
 				elif inpt == 10: #10
 					sumc -= 10
-					replace_card(10)
+					p_replace_card(10)
 					break
 				else: #other numbers
 					sumc += inpt
-					replace_card(inpt)
+					p_replace_card(inpt)
 					break
+	if sumc > 99:
+		raise SystemExit('You lose\n')
 	print('Sum: {}\n'.format(sumc))
-	print(pcard) #REMOVE LATER
 
+##bot plays
+def bot():
+	global bcard
+	global cardl
+	global sumc
+	nums = [2, 3, 5, 6, 7, 8]
+	power = True #does the bot have all power cards?
+	if sumc <= 88:
+		for i in bcard:
+			if i in nums:
+				power = False
+				break
+		if not power:
+			while True:
+				power = bcard[random.randint(0, cardn - 1)]
+				if power in nums:
+					sumc += power
+					print('Card played: {}\nSum: {}\n'.format(power, sumc))
+					break
+		else:
+			power = bcard[random.randint(0, cardn - 1)]
+			if power == 1:
+				inpt = random.randint(1, 2)
+				if inpt == 1:
+					sumc += 1
+					print('Card played: A(1)\nSum: {}\n'.format(sumc))
+				else:
+					sumc += 11
+					print('Card played: A(11)\nSum: {}\n'.format(sumc))
+			else:
+				if power == 10:	
+					sumc -= 10
+					print('Card played: 10\nSum: {}\n'.format(sumc))
+				elif power == 11 or power == 12:
+					sumc += 10
+					if power == 11:
+						print('Card played: J\nSum: {}\n'.format(sumc))
+					else:
+						print('Card played: Q\nSum: {}\n'.format(sumc))
+				else:
+					sumc = 99
+					if power == 13:
+						print('Card played: K\nSum: {}\n'.format(sumc))
+					else:
+						print('Card played: Joker\nSum: {}\n'.format(sumc))
+		b_replace_card(power)
+	else:
+		played = False #did bot play yet?
+		for i in bcard:
+			if i in nums and i + sumc <= 99:
+				sumc += i
+				print('Card played: {}\nSum: {}\n'.format(i, sumc))
+				played = True
+		if not played:
+			if sumc + 10 <= 99:
+				if 11 in bcard or 12 in bcard:
+					sumc += 10
+					if 11 in bcard:
+						i = 11
+						print('Card played: J\nSum: {}\n'.format(sumc))
+					else:
+						i = 12
+						print('Card played: Q\nSum: {}\n'.format(sumc))
+					played = True
+		if not played:
+			if 1 in bcard and sumc < 99:
+				sumc += 1
+				print('Card played: A(1)\nSum: {}\n'.format(sumc))
+			else:
+				if 13 in bcard or 14 in bcard:
+					sumc = 99
+					if 13 in bcard:
+						i = 13
+						print('Card played: K\nSum: {}\n'.format(sumc))
+					else:
+						i = 14
+						print('Card played: Joker\nSum: {}\n'.format(sumc))
+				elif 4 in bcard:
+					i = 4
+					print('Card played: 4\nSum: {}\n'.format(sumc))
+				elif 9 in bcard:
+					i = 9
+					print('Card played: 9\nSum: {}\n'.format(sumc))
+				elif 10 in bcard:
+					sumc -= 10
+					i = 10
+					print('Card played: 10\nSum: {}\n'.format(sumc))
+				else:
+					raise SystemExit('You win!')
+		b_replace_card(i)
+				
 ##turn test
 def play(n):
 	if n == 0:
@@ -145,3 +251,9 @@ else:
 while True:
 	inpt += 1
 	play(inpt % 2)
+	if cardl == []: #if cards all used up recycle deck
+		for i in range(1, 14): #TODO work on making recycled deck not have duplicates
+			for k in range(4):
+				cardl.append(i)
+		for i in range(2):
+			cardl.append(14)
