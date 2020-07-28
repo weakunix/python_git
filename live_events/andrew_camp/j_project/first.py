ed4c370f85d51085c436a83a50ec6c755c155538
Cowland <kingofcowlandmrmoooo@gmail.com>
2020-07-28 09:27:38 -0400

    dotter

--- live_events/andrew_camp/j_project/first.py
+++ live_events/andrew_camp/j_project/first.py
@@ -1,8 +1,17 @@
-from sympy import *
-x = Symbol('x')
-y = Symbol('y')
-expr = 20*x - 15*y
-print(sympify(expr))
+import math
 
-expr2 = x**2/2+y/3
-pprint(expr2)
+def mandle(a,b,c,d,n):
+    real = c**2-d**2+a
+    img = 2*c*d+b
+    c=real
+    d = img
+    if n>1 and distance(c,d) < 2:
+        return mandle(a,b,c,d,n-1)
+    else:
+        return(c,d)
+
+def distance(a,b):
+    try:
+        return math.sqrt(a**2+b**2)
+    except:
+        return 2
