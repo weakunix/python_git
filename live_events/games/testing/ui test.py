050444b97187d85864fbca50c1b0c6793ab71f17
Cowland <kingofcowlandmrmoooo@gmail.com>
2020-07-30 12:54:30 -0400

    sdf

--- live_events/games/testing/ui test.py
+++ live_events/games/testing/ui test.py
@@ -1,1 +1,17 @@
-import turtle
+import tkinter as tk
+from tkinter import messagebox # messagebox must be explicitly imported
+
+# make top window
+top = tk.Tk()
+top.geometry("200x200")
+
+# button callback
+
+# place button
+B = tk.Button(top, text = "SinglePlayer")
+B.place(x = 80, y = 80)
+D = tk.Button(top, text = "MultiPlayer")
+D.place(x = 160, y = 80)
+
+# launch main loop
+top.mainloop()
