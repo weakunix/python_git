6b7bc9b1002424cd5f44c678a4011fda7878c715
Cowland Mootbing <>
2020-08-11 22:35:21 -0400

    stocksim doesnt work well with tkinter

--- live_events/games/testing/stocksim.py
+++ live_events/games/testing/stocksim.py
@@ -1,6 +1,39 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import random
+import threading
+import tkinter as tk
+
+window = tk.Tk()
+window.geometry("800x600")
+change = random.uniform(-0.2, 0.2)
+percentinvestedat = 0
+
+
+def invest():
+    global percentinvestedat
+    percentinvestedat = change + value
+
+
+def pull():
+    global percentinvestedat
+    gained = (change + value) - percentinvestedat
+    print("gained" + str(gained))
+    print("Git pullOrcadadle! You have invested in this stock at " + str(percentinvestedat) + "now it is worth" + str(
+        (change + value)))
+
+
+def main():
+    b = tk.Button(window, text="Hit it!", command=invest)
+    b.place(relx=0.5, rely=0.6)
+    c = tk.Button(window, text="I'll stand and retract!", command=pull)
+    c.place(relx=0.6, rely=0.6)
+    a = tk.Label(window, text="Stalk simulator, numbers change by the second. goodluck investor!")
+    a.place(relx=0.5, rely=0.5)
+    ani = animation.FuncAnimation(fig, animate, interval=1000)
+    plt.show()
+    window.after(1000, main())
+
 
 fig = plt.figure()
 ax1 = fig.add_subplot(1, 1, 1)
@@ -15,6 +48,8 @@ def animate(i):
     global yar
     global counter
     global value
+    global change
+    change = random.uniform(-0.2, 0.2)
     if len(xar) >= 10:
         xar.remove(xar[0])
     if len(yar) >= 10:
@@ -26,11 +61,12 @@ def animate(i):
     elif random.randint(0, 100) == 11:
         value /= divider
     xar.append(counter)
-    yar.append(random.uniform(-0.05, 0.05) + value)
+    yar.append(change + value)
     ax1.clear()
     ax1.plot(xar, yar)
     counter += 1
 
 
-ani = animation.FuncAnimation(fig, animate, interval=1000)
-plt.show()
+if __name__ == '__main__':
+    main()
+    window.mainloop()
