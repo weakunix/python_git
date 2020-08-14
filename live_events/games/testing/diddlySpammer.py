68e44f178107f8fa48bac13d3290d6d2480c799f
Cowland Mootbing <>
2020-08-14 19:44:14 -0400

    discord spammer

--- live_events/games/testing/diddlySpammer.py
+++ live_events/games/testing/diddlySpammer.py
@@ -1,12 +1,21 @@
-import pynput
+import string
 import keyboard
+import random
+import time
 from pynput.keyboard import Key, Controller
 
-dict = {
-
-}
+alphabet = string.ascii_uppercase
+alphabet = list(alphabet)
 keyboardsim = Controller()
-while True:
+keeprunning = True
+
+while keeprunning:
     if keyboard.is_pressed('q'):
-        #for i in range(1000):
-        keyboardsim.type("\n")
+        for i in range(100):
+            a = []
+            for er in range(random.randint(1, 5)):
+                a.append(alphabet[random.randint(0, len(alphabet) - 1)])
+                keyboardsim.type(str(a[er]))
+            keyboardsim.type("\n")
+        keeprunning = False
+keyboardsim.type("This was typed using a Python script. How does it feel to get beaten by a bot? ;)\n")
