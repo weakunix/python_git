0bc8d62acf6673ba63a41df073dae8a86dcfc060
Cowland Mootbing <>
2020-08-15 19:23:28 -0400

    bot code v0.3

--- live_events/games/tacobot/tacoshacknowait.py
+++ live_events/games/tacobot/tacoshacknowait.py
@@ -4,7 +4,7 @@ import time
 import math as albreto
 from pynput.keyboard import Controller, Listener, Key
 
-# version 0.3
+# version 0.2
 print('r-cmd to start cheet')
 alphabet = list(string.ascii_uppercase)
 keyboardsim = Controller()
@@ -34,32 +34,31 @@ def start(timetoadd):
     timewait = 300
     randomdivision = random.randint(1, 40)
     if i != 0:
-        timewait = albreto.ceil(((300 + random.randint(0,
-                                                       1)) + timetoadd) / randomdivision)  # random delay from 10seconds to 9 minutes to confirm anti-bot
+        timewait = albreto.ceil(((300 + random.randint(0, 1)) + timetoadd) / randomdivision)  # random delay from 10seconds to 9 minutes to confirm anti-bot
         timewait = int(timewait)
         print(timewait * randomdivision)
     exampl = 0
     for distractlesgo in range(randomdivision):
-        keyboardsim.type(str(botconfusion[random.randint(0, len(botconfusion) - 1)]) + "\n")
+        print(str(botconfusion[random.randint(0, len(botconfusion) - 1)]) + "\n")
         time.sleep(timewait)
         exampl += timewait
     print(exampl)
-    dayfromnow -= (timewait * randomdivision)
+    dayfromnow -= timewait * randomdivision
     a.write("days time: "+str(dayfromnow))
     if i == 0:
-        keyboardsim.type("%" + command[1][0])  # work if it is 10 mins
+        print("%" + command[1][0])  # work if it is 10 mins
         a.write("command: "+command[1][0])
-        time.sleep(random.randint(4, 30))
-    keyboardsim.type("%" + command[0][0])  # tips every 5 mins or so
+    print("%" + command[0][0])  # tips every 5 mins or so
     a.write("command: "+command[0][0])
     if dayfromnow <= 0:
         for i in range(0, 2):
-            time.sleep(random.randint(5, 10))
-            keyboardsim.type("%" + command[2 + i][0])
+            #time.sleep(random.randint(5, 10))
+            print("%" + command[2 + i][0])
             a.write("DAILYS: " + command[2+i][0])
         dayfromnow = (86400 + random.randint(100, 300))
-    a.close()
     counter += 1
+    timetoadd = 0
+    a.close()
     start(0)
 
 
@@ -67,12 +66,9 @@ def on_press(key):  # using cmd and ctrl bc they cant be repeated (smort)
     print("key pressed" + str(key))
     if key == Key.cmd_r or key == Key.ctrl_r:
         print("starting... in 10-50 seconds")
-        a = 0
         for i in range(0, len(command)):
-            a = random.randint(10, 49)
-            time.sleep(a)
-            keyboardsim.type("%" + command[i][0])
-        start(a)
+            print("%" + command[i][0])
+        start(0)
         # pass
 
 
