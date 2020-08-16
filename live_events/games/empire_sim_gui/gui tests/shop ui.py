54178d62f8139bfd757c0d56bc69dfa1635a5b26
Cowland Mootbing <>
2020-08-16 13:27:27 -0400

    working shop gui

--- live_events/games/empire_sim_gui/gui tests/shop ui.py
+++ live_events/games/empire_sim_gui/gui tests/shop ui.py
@@ -1,4 +1,8 @@
+import random
 import tkinter as tk
+from PIL import Image, ImageTk
+from functools import partial
+from tkinter import messagebox
 from tkinter import messagebox  # messagebox must be explicitly imported
 
 # make window window
@@ -10,40 +14,97 @@ window.title("royadle")
 solddata = [  # example of what sold in format
     ["Cowland", "Materials", 1000, 10],  # castle from, what sold, how many, money per
     ["Eagle Claw Nation", "Food", 10, 10],  # castle from, what sold, how many, money per
-    [False, "Magic Fragment", 1, 1000]  # no castle (wandering trader), selling magic fragment(1) for (1000)
+    [False, "Fragment", 1, 1000]  # no castle (wandering trader), selling magic fragment(1) for (1000)
 ]
+imagesforgame = []
+
+
+def imageload():
+    global imagesforgame
+    imagesforgame = []
+    for load in range(26, 27):
+        nameoffile = ("./shittyworldgenimg/", str(load), ".png")
+        nameoffile = "".join(nameoffile)
+        load = Image.open(nameoffile)
+        load = load.resize((100, 100))
+        imagestuff = ImageTk.PhotoImage(load)
+        imagesforgame.append(imagestuff)
+
+
+imageload()
 
 
 # button destroy
-def destroybtn(*destroys):
+def destroybtn(*destroys, **kwargs):
+    delsarry = kwargs.get("ary", None)
+    try:
+        for i in range(len(delsarry)):
+            delsarry[i].destroy()
+    except:
+        pass
     for args in destroys:
         args.destroy()
 
 
 # place button
 def pageo():
+    delarray = []
     whereToPlace = [
         [200, 75],
         [400, 75],
         [200, 300],
-        [400, 300]
+        [400, 300],
+        [0, 75],
+        [600, 75],
+        [0, 300],
+        [600, 300]
     ]
+    whereToPlacetxt = [
+        [25, 2],
+        [25, 120],
+        [10, 145],
+        [10, 170],
+        [120, 185]
+    ]
+    for i in range(len(solddata)):
+        texts = [
+            solddata[i][0],
+            solddata[i][1],
+            ("Amount:" + str(solddata[i][2]) + " $" + str(solddata[i][3])),
+            0,
+            "Buy"
+        ]
+        offer = tk.Button(window, text="", width=17, height=13)
+        offer.place(x=whereToPlace[i][0], y=whereToPlace[i][1])
+        offerimg = tk.Button(window, image=imagesforgame[0])
+        offerimg.place(x=whereToPlace[i][0] + 25, y=whereToPlace[i][1] + 20)
+        for b in range(0, 5):
+            if b == 3:
+                howmuch = tk.Scale(window, from_=0, to=solddata[i][2], orient=tk.HORIZONTAL)
+                howmuch.place(x=whereToPlace[i][0] + whereToPlacetxt[b][0],
+                              y=whereToPlace[i][1] + whereToPlacetxt[b][1])
+                delarray.extend((offerimg, offer, howmuch))
+            else:
+                info = tk.Label(window, text=texts[b])
+                info.place(x=whereToPlace[i][0] + whereToPlacetxt[b][0], y=whereToPlace[i][1] + whereToPlacetxt[b][1])
+                delarray.extend((offerimg, offer, info))
     offersText = tk.Label(window, text="Offers:")
     offersText.config(font=("Courier", 44))
     offersText.place(x=400, y=25, anchor=tk.CENTER)
-    Home = tk.Button(window, text="Home", width=7, height=2, highlightbackground='#FF0000', fg='#000000')
+    Home = tk.Button(window, text="Home", width=7, height=2, highlightbackground='#FF0000', fg='#000000',
+                     command=lambda: [quit(0)])  # TODO RUOYU CHANGE THIS
     Home.place(x=0, y=0, anchor=tk.NW)
     nexxt = tk.Button(window, text=">",  # make load button
-                      command=lambda: [paget(), destroybtn(nexxt, Home)])
+                      command=lambda: [paget(), destroybtn(nexxt, Home, offersText, ary=delarray)])
     nexxt.place(x=500, y=550, anchor=tk.CENTER)
-    for i in range(len(solddata)):
-        offer = tk.Button(window, text="", width=20, height=12)
-        offer.place(x=whereToPlace[i][0], y=whereToPlace[i][1])
 
 
 def paget():
+    Home = tk.Button(window, text="Home", width=7, height=2, highlightbackground='#FF0000', fg='#000000',
+                     command=lambda: [quit(0)])  # TODO RUOYU CHANGE THIS
+    Home.place(x=0, y=0, anchor=tk.NW)
     last = tk.Button(window, text="<",  # make delete save button
-                     command=lambda: [pageo(), destroybtn(last)])
+                     command=lambda: [pageo(), destroybtn(last, Home)])
     last.place(x=300, y=550, anchor=tk.CENTER)
 
 
