162ab264374baf6fb249472ede73bd9bad7fd780
Cowland Mootbing <>
2020-09-08 23:12:33 -0400

    rid ; from dead projects (tsktsk jason bad habit)

--- inactive/ProjectChatty/host.py
+++ inactive/ProjectChatty/host.py
@@ -7,14 +7,14 @@ import os
 #files:
 import send_recv
 
-external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8');
-name = input("what is your name");
-name1 = "";
-namething = '';
-conn = '';
-port = 12345;
-theirEIP = "";
-sendReadAlerts = "";
+external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+name = input("what is your name")
+name1 = ""
+namething = ''
+conn = ''
+port = 12345
+theirEIP = ""
+sendReadAlerts = ""
 def get_ip():
   s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
   try:
@@ -26,69 +26,69 @@ def get_ip():
   finally: 
     s.close()
   return IP
-c=0;
+c=0
 def setupH():
-    global name;
-    global port;
-    global external_ip;
-    global name1;
-    global namething;
-    global conn;
-    global sendReadAlerts;
-    global theirEIP;
-    port = int(input("port?"));
-    s = socket.socket();
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1);
-    host = get_ip();#
-    #host = external_ip;
+    global name
+    global port
+    global external_ip
+    global name1
+    global namething
+    global conn
+    global sendReadAlerts
+    global theirEIP
+    port = int(input("port?"))
+    s = socket.socket()
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    host = get_ip()#
+    #host = external_ip
     s.bind((host,port))
-    print("logged in on local ip:",host);
-    print("\nglobal IP:",external_ip);
-    print(port);
-    print("\nsuccessfully connected \n waiting for connections\n cancel?");
+    print("logged in on local ip:",host)
+    print("\nglobal IP:",external_ip)
+    print(port)
+    print("\nsuccessfully connected \n waiting for connections\n cancel?")
     #pplJoin = threading.Thread(target=)
-    s.listen(1);
-    conn, adr = s.accept();
+    s.listen(1)
+    conn, adr = s.accept()
     #send files
-    name = name.encode();
-    conn.sendall(name);
-    name = name.decode();
-    name1 = conn.recv(1024);
-    name1 = name1.decode();#
-    theirIP = conn.recv(1024);
-    theirIP=theirIP.decode();#
-    print(theirIP);
-    theirEIP = conn.recv(1024);
-    theirEIP = theirEIP.decode();
-    print(theirEIP);
+    name = name.encode()
+    conn.sendall(name)
+    name = name.decode()
+    name1 = conn.recv(1024)
+    name1 = name1.decode()#
+    theirIP = conn.recv(1024)
+    theirIP=theirIP.decode()#
+    print(theirIP)
+    theirEIP = conn.recv(1024)
+    theirEIP = theirEIP.decode()
+    print(theirEIP)
     temptuple = ("convos",str(datetime.datetime.now()),".txt")
-    namething = str("".join(temptuple));
-    print(namething);
-    namething = namething.replace(' ','_');
-    namething = namething.replace(':','_');
-    print(namething);
-    h=open(namething,"w+");
-    temptuple1 = ("From HOST, on port: ",str(port),": conversation between ",name," (",host,")(",external_ip,") and ",name1," (",theirIP,") (",theirEIP,")\n ================= \n");
+    namething = str("".join(temptuple))
+    print(namething)
+    namething = namething.replace(' ','_')
+    namething = namething.replace(':','_')
+    print(namething)
+    h=open(namething,"w+")
+    temptuple1 = ("From HOST, on port: ",str(port),": conversation between ",name," (",host,")(",external_ip,") and ",name1," (",theirIP,") (",theirEIP,")\n ================= \n")
     #temptuple1 = ("conversation between ",name," (",host,") and ",name1," (",theirIP,") \n ================= \n")
-    temptuple1= "".join(temptuple1);
-    h.write(str(temptuple1));
+    temptuple1= "".join(temptuple1)
+    h.write(str(temptuple1))
     h.close()
     os.system('clear')
-    print(theirIP," known as ",name1," Joined the server!\n=======Talk======\n");
+    print(theirIP," known as ",name1," Joined the server!\n=======Talk======\n")
     sendReadAlerts = input("send read alerts? (yes,no)")
     if(sendReadAlerts != "yes" and sendReadAlerts != "no"):
-      print("yes or no dumbo! defaulted to no");
-      sendReadAlerts = "no";
+      print("yes or no dumbo! defaulted to no")
+      sendReadAlerts = "no"
     return name,name1
 while c!=-1:
   while c==0:
-    time.sleep(1);
-    setupH();
+    time.sleep(1)
+    setupH()
     c=1
   while c==1:
     if(send_recv.sendMsg(conn,namething,name,name1,sendReadAlerts,theirEIP) == 0 and c == 1):
-      c=0;
+      c=0
       break
     if(send_recv.recvMsg(conn,namething,name,name1,sendReadAlerts) == 0 and c==1):
-      c=0;
+      c=0
       break
