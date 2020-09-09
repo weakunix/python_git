cf14a3d96862425fccd973605c4936fa0d81f2e7
Cowland Mootbing <>
2020-09-08 23:07:03 -0400

    ch18 wip

---
+++ live_events/slitherintopy/chapter18/line.py
@@ -0,0 +1,12 @@
+class Line:
+    def __init__(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = x1
+        self.y2 = x2
+    def __len__(self):
+        x = self.x2 - self.x1 
+        y = self.y2 - self.y1
+        return 
+    def __add__(self, otherx, othery):
+        
