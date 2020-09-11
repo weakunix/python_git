a219e27e6dcc2d78015456c5cd63b0a9c17b609e
Cowland Mootbing <>
2020-09-11 16:21:32 -0400

    kivy is bad

--- inactive/kivytitoral/kivy4.py
+++ inactive/kivytitoral/kivy4.py
@@ -5,11 +5,15 @@ from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 
-class DZNR(GridLayout):
-    pass
-
-        
+class DZNR(Widget):
+    name = ObjectProperty(None)
+    email = ObjectProperty(None)
+     #used in kv file
+    def press(self):
+        print("Name: "+str(self.name.text)+"\nEmail: "+str(self.email.text))
 
 class MyApp(App):
     def build(self):
