df5fd5a7d005fa1194cef5964bf498ff2e946689
Cowland Mootbing <>
2020-09-19 16:45:08 -0400

    minor change

--- live_events/testing/webscrapernodownload.py
+++ live_events/testing/webscrapernodownload.py
@@ -2,9 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-parafo = BeautifulSoup(requests.get("https://randomtextgenerator.com/").content, 'html.parser').find_all("textarea", id="generatedtext")
+'''
+Thanks to:
+https://randomtextgenerator.com/ 
+for the paragraphs generated!
+'''
 
-parafoRegExed = re.sub('</textarea>]', "", str(re.sub('\[<textarea id="generatedtext">', "", str(parafo))))
+parafoRegExed = re.sub('</textarea>]', "", str(re.sub('\[<textarea id="generatedtext">', "", str(BeautifulSoup(requests.get("https://randomtextgenerator.com/").content, 'html.parser').find_all("textarea", id="generatedtext")))))
 #sounds like fed-ex IK lol
 
 print(parafoRegExed)
