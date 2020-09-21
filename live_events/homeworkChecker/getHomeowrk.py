2d9df9bb5af511797bdbfd10007be30d26eebcd6
Cowland Mootbing <>
2020-09-21 17:42:56 -0400

    homework getter workssss

--- live_events/homeworkChecker/getHomeowrk.py
+++ live_events/homeworkChecker/getHomeowrk.py
@@ -2,28 +2,28 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-'''
-parafoRegExed = re.sub('</div>]', "", str(re.sub('\[<div class="fc-day-number">', "", str(BeautifulSoup(requests.get("https://rutgersprep.myschoolapp.com/app/student#studentmyday/schedule").content, 'html.parser')))))
+sup = BeautifulSoup(requests.get("https://calendar.google.com/calendar/u/0/htmlembed?src=a7agc1d6cg82r3on8m6q9ifoqo7bf2l2@import.calendar.google.com&amp;ctz=America/New_York").content, 'html.parser') 
 
+assignment = []
+classNames = []
+todo = {}
 schedules = []
-'''
 
-todo = {
-	"English": ["analysis da text", "tonite"], #naibing class: have to do cows, due mootbing
-	"Math": ["do yah algeeebruh", "next wek"]
-}
+for event in sup.find_all("span", class_="event-summary"):
+	if event != None:
+		event = str(event).replace('<span class="event-summary">', "").replace("</span>", "").replace("&amp;", "&")
+		chars = []
+		i = 0
+		while "-" not in chars:
+			chars.append(event[i])
+			i += 1
+		classNames.append("".join(chars).replace("-", ""))
+		assignment.append(event.replace("".join(chars), ""))
 
-'''
-
-print("Your today schedule:\n " + str(schedules).replace("[", "").replace("]", ""))
-
-print("TODO: \n" + str(todos).replace("[", "").replace("]", "")
-
+for i in range(len(assignment)):
+	todo[classNames[i]] = [assignment[i], "a"]
+#print("Your today schedule:\n " + str(schedules).replace("[", "").replace("]", ""))
 #sounds like fed-ex IK lol
-
-print(parafoRegExed)
-'''
-
 for className in todo:
 	print("\nYou have assignment in \nClass: " + str(className) + "\nDesc: " + str(todo[str(className)][0]) + "\nDue on: " + str(todo[str(className)][1]))
 
