76b660e7c9951d24ddee855e10d0e9b1021e1ff9
Cowland Mootbing <>
2020-09-21 21:51:26 -0400

    tacobot with %daily and clean

--- live_events/discord_bots/tacobot/tacoshak.py
+++ live_events/discord_bots/tacobot/tacoshak.py
@@ -27,32 +27,38 @@ with open('distract.txt', 'r') as b:
 
 
 def start(timetoadd):
-    global counter
-    b = open("documentation.txt", "w+")
-    b.write("")
-    b.close()
-    a = open("documentation.txt", "a+")
-    randomdivision = 3
-    timewait = 120
-    print(albreto.ceil(((300 + random.randint(0,
-                                                   100)) + timetoadd) / randomdivision))  # random delay from 10seconds to 9 minutes to confirm anti-bot
-    timewait = int(timewait)
-    print(timewait * randomdivision)
-    exampl = 0
-    for distractlesgo in range(randomdivision):
-        keyboardsim.type(str(botconfusion[random.randint(0, len(botconfusion) - 1)]) + "\n")
-        time.sleep(timewait)
-        exampl += timewait
-    if counter % 2 == 0:
-        keyboardsim.type("%" + command[1][0])  # work if it is 10 mins
-        time.sleep(random.randint(4, 30))
-    keyboardsim.type("%" + command[0][0])  # tips every 5 mins or so
-    if counter % 6 == 0:
-        time.sleep(3)
-        keyboardsim.type("%" + command[4][0])  # work if it is 10 mins
-        time.sleep(random.randint(4, 30))
-    counter += 1
-    start(0)
+    while True:
+        global counter
+        b = open("documentation.txt", "w+")
+        b.write("")
+        b.close()
+        a = open("documentation.txt", "a+")
+        randomdivision = 3
+        timewait = 120
+        print(albreto.ceil(((300 + random.randint(0,
+                                                    100)) + timetoadd) / randomdivision))  # random delay from 10seconds to 9 minutes to confirm anti-bot
+        timewait = int(timewait)
+        print(timewait * randomdivision)
+        exampl = 0
+        for distractlesgo in range(randomdivision):
+            keyboardsim.type(str(botconfusion[random.randint(0, len(botconfusion) - 1)]) + "\n")
+            time.sleep(timewait)
+            exampl += timewait
+        if counter % 2 == 0:
+            keyboardsim.type("%" + command[1][0])  # work if it is 10 mins
+            time.sleep(random.randint(4, 30))
+        if counter % 6 == 0:
+            keyboardsim.type("%" + command[4][0])  # work if it is 10 mins
+            time.sleep(random.randint(4, 30))
+        if counter % 1440 == 0:
+            for i in range(2, len(command) - 1):
+                a = random.randint(10, 20)
+                time.sleep(a)
+                keyboardsim.type("%" + command[i][0])
+            time.sleep(random.randint(4, 30))
+        keyboardsim.type("%" + command[0][0])  # tips every 5 mins or so
+
+        counter += 1
 
 
 def on_press(key):  # using cmd and ctrl bc they cant be repeated (smort)
--- live_events/homeworkChecker/getHomeowrk.py
+++ live_events/homeworkChecker/getHomeowrk.py
@@ -1,29 +1,105 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import os
+																																		#v here add html instead of just embed to get html version of the calendar
+todoScraped = BeautifulSoup(requests.get("https://calendar.google.com/calendar/u/0/htmlembed?src=a7agc1d6cg82r3on8m6q9ifoqo7bf2l2@import.calendar.google.com&amp;ctz=America/New_York").content, 'html.parser') 
+scheduleScraped = BeautifulSoup(requests.get("https://calendar.google.com/calendar/u/0/htmlembed?src=vt09lfemhmgtajic4dvmou1u5jcaueef@import.calendar.google.com&amp;ctz=America/New_York").content, 'html.parser') 
 
-sup = BeautifulSoup(requests.get("https://calendar.google.com/calendar/u/0/htmlembed?src=a7agc1d6cg82r3on8m6q9ifoqo7bf2l2@import.calendar.google.com&amp;ctz=America/New_York").content, 'html.parser') 
+def clear():
+	if os.name == 'nt': 
+		os.system('cls') #windows
+	elif os.name == 'darwin':
+		os.system('clear') 
+	elif os.name == 'posix':
+		try:
+			import console; console.clear() #pythonista
+		except:
+			pass
+		else:
+			return
+		try:
+			os.system('clear') #mac/linux
+		except:
+			pass
+	else: 
+	    os.system('clear') #mac/linux
 
-assignment = []
-classNames = []
-todo = {}
-schedules = []
+def calcTodo():
+	clear()
+	sub = []
+	desc = []
+	for event in todoScraped.find_all("span", class_="event-summary"):
+		if event != None:
+			event = str(event).replace('<span class="event-summary">', "").replace("</span>", "").replace("&amp;", "&")
+			chars = []
+			i = 0
+			while "-" not in chars:
+				chars.append(event[i])
+				i += 1
+			sub.append("".join(chars).replace("-", ""))
+			desc.append(event.replace("".join(chars), ""))
+	return sub, desc
 
-for event in sup.find_all("span", class_="event-summary"):
-	if event != None:
-		event = str(event).replace('<span class="event-summary">', "").replace("</span>", "").replace("&amp;", "&")
-		chars = []
-		i = 0
-		while "-" not in chars:
-			chars.append(event[i])
-			i += 1
-		classNames.append("".join(chars).replace("-", ""))
-		assignment.append(event.replace("".join(chars), ""))
+def calcSchedule():
+	clear()
+	sch = []
+	tim = []
+	cntr = 0
+	for classes in scheduleScraped.find_all("span", class_="event-summary"):
+		if classes != None:
+			classes = str(classes).replace('<span class="event-summary">', "").replace("</span>", "").replace("&amp;", "&")
+			if str(classes) not in sch and cntr > 5:
+				sch.append(str(classes))
+		cntr += 1
+	
+	for time in scheduleScraped.find_all("span", class_="event-time"):
+		if time != None:
+			time = str(time).replace('<span class="event-time">', "").replace("</span>", "").replace("&amp;", "&")
+			if time not in tim:
+				tim.append(time)
 
-for i in range(len(assignment)):
-	todo[classNames[i]] = [assignment[i], "a"]
+	return sch, tim
+
+def makedict(assignment, todo, classNames):
+	for i in range(len(assignment)):
+		todo[classNames[i]] = [assignment[i], "a"] #make new value in dict with about and due date
+	return todo
 #print("Your today schedule:\n " + str(schedules).replace("[", "").replace("]", ""))
 #sounds like fed-ex IK lol
-for className in todo:
-	print("\nYou have assignment in \nClass: " + str(className) + "\nDesc: " + str(todo[str(className)][0]) + "\nDue on: " + str(todo[str(className)][1]))
 
+def display(todo):
+	for className in todo:
+		print("\nYou have assignment in \nClass: " + str(className) + "\nDesc: " + str(todo[str(className)][0]) + "\nDue on: " + str(todo[str(className)][1]))
+
+def displays(schedules, stime):
+	for i in range(len(stime)):
+		print(f"\nClass at {str(stime[i])}:\n {str(schedules[i])}")
+
+def main():
+	assignment = []
+	classNames = []
+	todo = {}
+	schedules = []
+	stime = []
+	while True:
+		clear()
+		sORt = input("\nschedule/todo/exit\n\n>>>")
+		if sORt.lower() == "schedule":
+			schedules, stime = calcSchedule()
+			displays(schedules, stime)
+			input("Enter to continue...")
+		elif sORt.lower() == "todo":
+			appends = calcTodo()
+			classNames = appends[0]
+			assignment = appends[1]
+			todo = makedict(assignment, todo, classNames)
+			display(todo)
+			input("Enter to continue...") 
+		elif sORt.lower() == "exit":
+			raise SystemExit("Done")
+		else:
+			print("Invalid Command!")
+
+if __name__ == "__main__":
+	main()
