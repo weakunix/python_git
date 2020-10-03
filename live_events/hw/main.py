cf1561559497493c06a37270b9fd0bc9a5868d5f
Cowland Mootbing <>
2020-10-03 18:52:02 -0400

    update

--- live_events/hw/main.py
+++ live_events/hw/main.py
@@ -1,12 +1,12 @@
-#imports
-import sys
-
 #IMPORT FILES
 from QTgenerated import application, login
 from handlers import filehandler, newhandler
+
+#other python imports
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QMessageBox
 import webbrowser
+import sys
 
 class loginPage():
     def __init__(self):
