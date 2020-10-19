9861df60f5eb0d1e5a5d52d73d72512633e99442
Cowland Mootbing <>
2020-10-18 23:31:27 -0400

    anti-cleb-bot

--- inactive/discord_bots/anti-cleb/main.py
+++ inactive/discord_bots/anti-cleb/main.py
@@ -9,12 +9,15 @@ client = discord.Client()
 
 @client.event
 async def on_message(message):
-    if message.embeds.title == "A trick-or-treater has stopped by!":
-        if "h!treat" in message.description:
-            keyboardsim.type("h!treat\n")
-        else:
-            keyboardsim.type("h!trick\n")
-        await asyncio.sleep(2)
-        keyboardsim.type("a\na\na\n")
+    try:
+        if message.embeds[0].title == "A trick-or-treater has stopped by!" and message.author.id == 755580145078632508:
+            if "h!treat" in message.embeds[0].description:
+                keyboardsim.type("h!treat\n")
+            else:
+                keyboardsim.type("h!trick\n")
+            keyboardsim.type("a\na\na\n")
+    except:
+        return
 
-client.login()
+print("stinky")
+client.run()
