bf1fd1fdc93e3e3385095fce6d014fa43fc29459
Cowland Mootbing <>
2020-10-21 10:24:13 -0400

    commentd

--- hw/handlers/sqlhandler.py
+++ hw/handlers/sqlhandler.py
@@ -20,15 +20,15 @@ def write(table, values, keys = ""):
         if keys != "":
             inject = (
                 f"INSERT INTO {table} ({keys}) VALUES ({values});"
-            )
+            ) #if there is a value to be ignored (eg, auto incriment)
         else:
             inject = (
                 f"INSERT INTO {table} VALUES ({values});"
-            )
+            ) #regular inject
         cursor.execute(inject) #injects username and password as new set in database
-        cnx.commit()
-        cleanUp(cnx, cursor)
-        return read(table, "*")
+        cnx.commit() #commit the injection
+        cleanUp(cnx, cursor) #closes all connections
+        return read(table, "*") #returns the new table's values (if needed)
     except Exception as e:
         cleanUp(cnx, cursor)
         print(e)
@@ -38,21 +38,21 @@ def read(table, selected, condition = "", order = ""):
     cnx = setup()
     cursor  = cnx.cursor()
     try:
-        query = (f"SELECT {selected} FROM {table} {condition} {order};")
-        cursor.execute(query)
-        arrayOfItems = []
+        query = (f"SELECT {selected} FROM {table} {condition} {order};") #conditon and order are "" on default so they won't impact anything but can be put in if needed
+        cursor.execute(query) #read
+        arrayOfItems = [] 
         for item in cursor:
             arrayOfItems.append(item)
-        cleanUp(cnx, cursor)
-        return arrayOfItems
+        cleanUp(cnx, cursor) #close all connections
+        return arrayOfItems #give back data in a array format
     except Exception as e:
         cleanUp(cnx, cursor)
         print(e)
         return False
 
 def checkForDuplicate(table, key, valueofdupe):
-    data = read(table, key, f'''WHERE {key} = "{valueofdupe}"''')
-    return True if data != [] else False
+    data = read(table, key, f'''WHERE {key} = "{valueofdupe}"''') #where the colomn has the duplicate value
+    return True if data != [] else False #if there is already then TRUE YOU HAVE DUPE else FALSE you don't
 
 if __name__ == "__main__":
     #arrayOfItems = read("kid", "*")
