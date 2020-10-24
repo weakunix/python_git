040630e4c9bcd9174a338f66554c9d9f92a25a1f
Cowland Mootbing <>
2020-10-24 17:22:30 -0400

    optomization

--- hw/handlers/themeshandler.py
+++ hw/handlers/themeshandler.py
@@ -2,10 +2,12 @@ from handlers import filehandler
 import yaml
 
 directory = "./QTgenerated/"
+defaultstyle = str(filehandler.yamlload("./data/settings.yaml")["theme"])
 
 def styleMain(ui):
-    defaultstyle = str(filehandler.yamlload("./data/settings.yaml")["theme"])
-    a = str((filehandler.yamlload(directory+"themes.yaml")[str(defaultstyle)]["todoDiv"]).replace(" ", "").replace('"', "")).replace("\\n", "").replace("'", "")
-    print(a)
-    ui.todoDiv.setStyleSheet(a)
-    #change later to have the 2 in settings.yaml
+    
+    ui.todoDiv.setStyleSheet(styleGetter("todoDiv"))
+    #change later to have the 2 in settings.
+
+def styleGetter(obj):
+    return str((filehandler.yamlload(directory+"themes.yaml")[str(defaultstyle)][str(obj)]).replace(" ", "").replace('"', "")).replace("\\n", "")
