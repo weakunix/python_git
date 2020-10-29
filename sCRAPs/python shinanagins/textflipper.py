1bd1eaa45f9aa7fabd07c162bba9d9520051bd31
Cowland Mootbing <>
2020-10-29 10:22:06 -0400

    flip text with f12

--- sCRAPs/python shinanagins/textflipper.py
+++ sCRAPs/python shinanagins/textflipper.py
@@ -2,6 +2,8 @@ from pynput.keyboard import Controller, Listener, Key
 import pyperclip
 import time
 
+alphabet = ["ɐ", "q", "ɔ", "p", "ǝ", "ɟ", "ƃ", "ɥ", "ᴉ", "ɾ", "ʞ", "l", "ɯ", " u", "o", "d", "b", "ɹ", "s", "ʇ", "n", "ʌ", "ʍ", "x", "ʎ", "z"]
+
 # how to setup your keyboard bc for control and whateves keys
 keyboardsim = Controller()
 value = None
@@ -23,11 +25,31 @@ def pastee(times):
     keyboardsim.type(str(value) * times)
     value = None
 
+def flip(text):
+    text = text.lower()
+    flipped = []
+    for i in text:
+        try:
+            flipped.append(alphabet[ord(i) - 97])
+        except:
+            flipped.append(" ")
+    flipped.reverse()
+    return "".join(flipped)
+
+def delAll():
+    keyboardsim.press(Key.cmd)
+    keyboardsim.press("a")
+    keyboardsim.release("a")
+    keyboardsim.release(Key.cmd)
+    keyboardsim.press(Key.backspace)
+    keyboardsim.release(Key.backspace)
+
 def on_release(key):
     global value
     if key == Key.ctrl_r:
         try:
-            pastee(int(copyy()) if int(copyy()) < 20 else 20)
+            maxvalue = 2000//len(value)
+            pastee(int(copyy()) if int(copyy()) < maxvalue else maxvalue - 1)
         except Exception as e:
             print(e)
             return
@@ -36,7 +58,10 @@ def on_release(key):
         value = copyy()
         print(value)
     elif key == Key.f12:
-        exit()
+        time.sleep(0.1)
+        value = flip(copyy())
+        delAll()
+        pastee(1)
     else:
         return
 
