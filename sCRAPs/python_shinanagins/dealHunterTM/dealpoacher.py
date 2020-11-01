1c32ccc8f8d91627f02ad0fa277aac709d162d7b
Cowland Mootbing <>
2020-11-01 17:14:16 -0500

    daelpoacher search

--- sCRAPs/python_shinanagins/dealHunterTM/dealpoacher.py
+++ sCRAPs/python_shinanagins/dealHunterTM/dealpoacher.py
@@ -21,7 +21,13 @@ def printFirstPage(isFile = False):
                 print("=")
     return dealstuff
 
-def query(query):
+def query(q):
+    req = "https://slickdeals.net/newsearch.php?src=SearchBarV2&q=" + q.replace(" ", "+")
+    site = BeautifulSoup(requests.get(str(req)).content, 'html.parser')
+    for item in site.find_all("p", {"class" : "head"}):
+        if item.get_text() == ("We found no results for "+str(q)+", please try again."):
+            return False #if there isn't any results for the  specified query
+    
     return #return the site queried
 
 def frontpage():
@@ -34,14 +40,31 @@ def frontpage():
             sickLinks.append(deal["href"].replace("/f/", "https://slickdeals.net/f/").replace("/coupons/", "https://slickdeals.net/coupons/"))
     return sickDeals, sickLinks #give back the array
 
-def main():
+def fp():
     printFirstPage()
-    if datetime.datetime.now().timestamp() - float(filehandler.yamlload("./data/settings.yaml")["LastLog"]) > 8.64e+7: #if a day has passed then show frontpage deals
+    if input("\n\n>>>save? (Y/N)\n>>>")[0] == "y":
         with open("./savedlist/deal_of_"+str(datetime.datetime.now()).replace(" ","_")+".txt", "w+") as f:
             for timedDeal in printFirstPage(True):
                 f.write(timedDeal + "\n")
-    filehandler.yaml_it("./data/settings.yaml", "LastLog", str(datetime.datetime.now().timestamp())) #stamp now login time
 
+def main():
+    if datetime.datetime.now().timestamp() - float(filehandler.yamlload("./data/settings.yaml")["LastLog"]) > 8.64e+7: #if a day has passed then show frontpage deals
+        fp()
+    filehandler.yaml_it("./data/settings.yaml", "LastLog", str(datetime.datetime.now().timestamp())) #stamp now login time
+    while True:
+        clearScreen()
+        command = input("Welcome to dealpoacher (not slickdeals ahem)\n Command list: \n type 'q' to quit\n type 'f' to display frontpage deals \n type 's item' to search all deals for item! \n>>>")
+        if command[0] == "q":
+            break
+        elif command[0] == "f":
+            fp()
+        elif command[0] == "s":
+            if query(command[2:]) != False:
+                pass
+            else:
+                input(f"\n We are sorry, but there doesn't seem to be any great results for '{str(command[2:])}' ... Check your spelling and capitalization. :( \n\n Enter to continue...")
+    clearScreen()
+    raise SystemExit ("Thanks for using dealpoacher.")
 
 if __name__ == '__main__':
     main() #start the deal hunt!
