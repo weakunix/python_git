55cc6a732d8e06f7837ad8a3391e37b9bb43d27a
CowlandonIpad <kingofcowlandmrmoooo@gmail.com>
2020-11-02 12:29:16 -0500

    Mobile widget issues

--- sCRAPs/python_shinanagins/dealpoacherTM/dealpoacher.py
+++ sCRAPs/python_shinanagins/dealpoacherTM/dealpoacher.py
@@ -27,6 +27,7 @@ import datetime
 from data import filehandler
 import math
 import time
+import platform
 
 def logo():
     for i in range(100):
@@ -40,17 +41,17 @@ def logo():
 {" " * ii} |    `   \  ___/ / __ \|  |_|    |  (  <_> ) __ \\  \___|   Y  \  ___/|  | \/
 {" " * ii}/_______  /\___  >____  /____/____|   \____(____  /\___  >___|  /\___  >__|   
 {" " * ii}        \/     \/     \/                        \/     \/     \/     \/       
-{" " * ii}                                      Designed & Developed in Cowland Software Technologies Lab™  ©1958 (yea right python wasn't even invented yet)
-        """)
+{" " * ii}                                      Designed & Developed in Cowland Software Technologies Lab™  ©1958 (yea right python wasn't even invented yet)""")
 
 def clearScreen():
     os.system('clear' if os.name =='posix' else 'cls')
+    print("\n"*100) #just in case
 
 def printFirstPage(isFile = False):
     dealstuff = []
     deal, link = frontpage()
     clearScreen()
-    for item in range(len(deal)):
+    for item in range(len(deal)): 
         if deal[item].replace(" ", "") != "":
             if isFile:
                 dealstuff.append("| Deal | " + deal[item] + " | " + link[item] + " |")
@@ -100,7 +101,8 @@ def fp():
             for timedDeal in printFirstPage(True):
                 f.write(timedDeal + "\n")
 
-def main():
+def consoleEdition():
+    
     if datetime.datetime.now().timestamp() - float(filehandler.yamlload("./data/settings.yaml")["LastLog"]) > 8.64e+7: #if a day has passed then show frontpage deals
         fp()
     filehandler.yaml_it("./data/settings.yaml", "LastLog", str(datetime.datetime.now().timestamp())) #stamp now login time
@@ -118,7 +120,7 @@ def main():
                 deal, link, times = q
                 dealstuff = []
                 item = 0
-                while item < len(deal if len(deal) == len(link) == len(times) else 0):
+                while item < len(deal if len(deal) == len(link) == len(times) else ""):
                     if deal[item].replace(" ", "") != "" and "Expired" not in times[item]:
                         dealstuff.append("| Deal | " + deal[item] + " | " + times[item] + " | " + link[item] + " |")
                         print(dealstuff[item])
@@ -133,6 +135,39 @@ def main():
     clearScreen()
     logo()
     print("Dealpoacher™ has been closed. You may now exit. \n\n THANK YOU FOR CHOOSING Dealpoacher™!\n\n ASCII art by (http://patorjk.com/software/taag/#p=display&f=Graffiti&t=DealPoacher) \n\n\n Check out other apps by Cowland Software Technologies Lab™ \n [Bingternet]()")
+
+def mobileEdition():
+    '''
+    SHORTCUTS = [
+    {'title': 'New Email', 'url': 'mailto:', 'color': '#5e96ff', 'icon': 'iow:email_24'},
+    {'title': 'New Message', 'url': 'sms://', 'color': '#5ec0ff', 'icon': 'iow:chatbox_24'},
+    {'title': 'Pythonista', 'url': 'pythonista3://', 'color': '#45d3e8', 'icon': 'iow:chevron_right_24'},
+    {'title': 'Forum', 'url': 'http://forum.omz-software.com', 'color': '#4dd19d'},
+    {'title': 'Google', 'url': 'http://google.com', 'color': '#a9de31'},
+    {'title': 'DuckDuckGo', 'url': 'http://duckduckgo.com', 'color': '#ffd026' },
+    {'title': 'Amazon', 'url': 'http://amazon.com', 'color': '#ff8e13'},
+    {'title': 'eBay', 'url': 'http://ebay.com', 'color': '#ff4a09'},
+    ]
+    '''
+    from versions import mobile, mobileWidget
+    deal, link = frontpage()
+    shortcuts = []
+    for item in range(len(deal) if len(deal) == len(link) else len("")):
+        shortcuts.append({"title":str(deal[item]), "url": str(link[item]), "color": "#5e96ff"})
+    mobileWidget.shortCut(shortcuts)
+    mobileWidget.main()
+    print("Widget Creation Successful!")
+    
+
+def main():
+    if platform.machine().startswith("iP"):
+        mobileEdition()
+    elif os.name() == "darwin":
+        macEdition()
+    elif os.name() == "posix":
+        winEdition()
+    else:
+        consoleEdition()
 
 
 if __name__ == '__main__':
---
+++ sCRAPs/python_shinanagins/dealpoacherTM/versions/mobileWidget.py
@@ -0,0 +1,63 @@
+#!python3
+
+'''
+used pythonista 3 example
+'''
+
+import appex, ui
+import os
+from math import ceil, floor
+
+# NOTE: The ROWS variable determines the number of rows in "compact" mode. In expanded mode, the widget shows all shortcuts.
+COLS = 3
+ROWS = 2
+
+# Each shortcut should be a dict with at least a 'title' and 'url' key. 'color' and 'icon' are optional. If set, 'icon' should be the name of a built-in image.
+SHORTCUTS = [
+{'title': 'New Email', 'url': 'mailto:', 'color': '#5e96ff', 'icon': 'iow:email_24'},
+{'title': 'New Message', 'url': 'sms://', 'color': '#5ec0ff', 'icon': 'iow:chatbox_24'},
+{'title': 'Pythonista', 'url': 'pythonista3://', 'color': '#45d3e8', 'icon': 'iow:chevron_right_24'},
+{'title': 'Forum', 'url': 'http://forum.omz-software.com', 'color': '#4dd19d'},
+{'title': 'Google', 'url': 'http://google.com', 'color': '#a9de31'},
+{'title': 'DuckDuckGo', 'url': 'http://duckduckgo.com', 'color': '#ffd026' },
+{'title': 'Amazon', 'url': 'http://amazon.com', 'color': '#ff8e13'},
+{'title': 'eBay', 'url': 'http://ebay.com', 'color': '#ff4a09'},
+]
+
+def shortCut(array):
+    global SHORTCUTS
+    SHORTCUTS = array 
+
+class LauncherView (ui.View):
+    def __init__(self, shortcuts, *args, **kwargs):
+        row_height = 110 / ROWS
+        super().__init__(self, frame=(0, 0, 300, ceil(len(shortcuts) / COLS) * row_height), *args, **kwargs)
+        self.buttons = []
+        for s in shortcuts:
+            btn = ui.Button(title=' ' + s['title'], image=ui.Image(s.get('icon', 'iow:compass_24')), name=s['url'],
+            action=self.button_action, bg_color=s.get('color', '#55bcff'), tint_color='#fff', corner_radius=9)
+            self.add_subview(btn)
+            self.buttons.append(btn)
+
+    def layout(self):
+        bw = self.width / COLS
+        bh = floor(self.height / ROWS) if self.height <= 130 else floor(110 / ROWS)
+        for i, btn in enumerate(self.buttons):
+            btn.frame = ui.Rect(i%COLS * bw, i//COLS * bh, bw, bh).inset(2, 2)
+            btn.alpha = 1 if btn.frame.max_y < self.height else 0
+
+    def button_action(self, sender):
+        import shortcuts
+        shortcuts.open_url(sender.name)
+
+def main():
+    widget_name = __file__ + str(os.stat(__file__).st_mtime)
+    v = appex.get_widget_view()
+    # Optimization: Don't create a new view if the widget already shows the launcher.
+    if v is None or v.name != widget_name:
+        v = LauncherView(SHORTCUTS)
+        v.name = widget_name
+        appex.set_widget_view(v)
+
+if __name__ == '__main__':
+    main()    
