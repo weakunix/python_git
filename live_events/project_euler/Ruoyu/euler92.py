5991911118cef6e677a5059792c314befbcc8500
Ruoyu Xu <ruoyu.xu2008@icloud.com>
2020-12-30 21:58:56 -0500

    Euler 92 complete

--- live_events/project_euler/Ruoyu/euler92.py
+++ live_events/project_euler/Ruoyu/euler92.py
@@ -1,19 +1,57 @@
 #vars
-lead_to_89 = []
-lead_to_1 = []
+LeadTo89 = {89}
+LeadTo1 = {1}
+TotalLeadTo89 = 0
+Count = 0
 
-#testing all numbers from 1 to 10 ** 7
-if __name__ == '__main__':
-    for i in range(1, 10 ** 7):
-        original = i
-        print(i)
-        while True:
-            if i in lead_to_89 or i == 89:
-                lead_to_89.append(original)
-                break
-            elif i in lead_to_1 or i == 1:
-                lead_to_1.append(original)
-                break
-            else:
-                i = sum([int(k) ** 2 for k in str(i)])
-    print(len(lead_to_89))
+#functions
+##sum of squares of digits
+def SumDigitSquares(n):
+    SumSquares = 0
+    while n > 0:
+        SumSquares += (n % 10) ** 2
+        n = int(n / 10)
+    return SumSquares
+
+##make number
+def MakeNumber(a, b, c, d, e, f, g):
+    n = 0
+    Multiply = 1
+    for i in [g, f, e, d, c, b, a]:
+        n += i * Multiply
+        Multiply *= 10
+    return n
+
+#trying all numbers
+for a in range(10):
+    for b in range(10):
+        for c in range(10):
+            for d in range(10):
+                for e in range(10):
+                    for f in range(10):
+                        for g in range(10):
+                            if Count != 0:
+                                if Count % 1000 == 0:
+                                    print(Count)
+                                DigitSum = sum([i ** 2 for i in [a, b, c, d, e, f, g]])
+                                if DigitSum in LeadTo89:
+                                    if Count <= 9 ** 2 * 7:
+                                        LeadTo89.add(Count)
+                                    TotalLeadTo89 += 1
+                                elif DigitSum in LeadTo1:
+                                    if Count <= 9 ** 2 * 7:
+                                        LeadTo1.add(Count)
+                                else:
+                                    if Count > 9 ** 2 * 7:
+                                        raise SystemExit('BUG')
+                                    while True:
+                                        DigitSum = SumDigitSquares(DigitSum)
+                                        if DigitSum in LeadTo89:
+                                            LeadTo89.add(Count)
+                                            TotalLeadTo89 += 1
+                                            break
+                                        elif DigitSum in LeadTo1:
+                                            LeadTo1.add(Count)
+                                            break
+                            Count += 1
+print(TotalLeadTo89)
