ab39e71d7a5a82c9ddeaa4d17ff7a5482807d202
R. Xu <ruoyu.xu2008@icloud.com>
2021-08-16 22:07:36 +0800

    Euler 206 complete

--- live_events/project_euler/Ruoyu/euler206.py
+++ live_events/project_euler/Ruoyu/euler206.py
@@ -2,25 +2,24 @@
 import numpy as np
 
 #Vars
-Digits = []
-Number = ''
+MinNum = int(np.sqrt(1020304050607080900))
+MaxNum = int(np.sqrt(1929394959697989900)) + 1
+
+#User def functions
+##Does square of a number meet criteria
+def IsCorrectSquare(x):
+    x = str(x ** 2)
+    for i in range(0, len(x), 2):
+        if str((int(i / 2) + 1) % 10) != x[i]:
+            return False
+    return True
 
 #Test numbers
 if __name__ == '__main__':
-    for a in range(10):
-        for b in range(10):
-            for c in range(10):
-                for d in range(10):
-                    for e in range(10):
-                        for f in range(10):
-                            for g in range(10):
-                                for h in range(10):
-                                    Digits = [a, b, c, d, e, f, g, h, 0]
-                                    if sum(Digits) % 9 == 0:
-                                        Number = ''
-                                        for i in range(9):
-                                            Number += str(i + 1) + str(Digits[i])
-                                        Number = np.sqrt(int(Number + '0'))
-                                        if Number == int(Number):
-                                            print(Number)
-                                            raise SystemExit() 
+    print(MinNum, MaxNum)
+    for i in range(MinNum, MaxNum, 10): #MinNum happens to be divisible by 10
+        if i % 1000000 == 0:
+            print(i)
+        if IsCorrectSquare(i):
+            print(i)
+            raise SystemExit()
