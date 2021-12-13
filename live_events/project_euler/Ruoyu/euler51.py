2b75fac13ac6102cf96c7e98cb62620e8e47fa19
MrMooooYT <mrmooooyt@gmail.com>
2021-12-13 20:12:16 +0000

    Euler 51 incomplete

--- live_events/project_euler/Ruoyu/euler51.py
+++ live_events/project_euler/Ruoyu/euler51.py
@@ -2,9 +2,8 @@
 import numpy as np
 
 #Vars
-ToTestNums = []
-ToTestDigits = []
 Primes = []
+PossibleNums = []
 CurrentPow = 0
 
 #User def functions
@@ -21,29 +20,19 @@ def IsPrime(n):
 
 def IsPossibleNum(n):
     strn = str(n)
-    for digit in range(10):
-        Occurence = strn.count(str(digit))
-        if Occurence > 0 and Occurence % 3 == 0:
-            ToTestNums.append(n)
-            ToTestDigits.append(digit)
+    for i in range(10):
+        pvlist = [] #place value list
+        if strn.count(str(i)) % 2 == 0: #CHANGE TO 3
+            for pv, digit in enumerate(strn):
+                if digit == str(i):
+                    pvlist.append(pv)
+            pass
 
 #Test
 while True:
     CurrentPow += 1
-    ToTestNums = []
-    ToTestDigits = []
+    PossibleNums = []
     for i in range(10 ** (CurrentPow - 1), 10 ** CurrentPow + 1):
         if IsPrime(i):
             Primes.append(i)
-            IsPossibleNum(i)
             print(i)
-    for i in range(len(ToTestNums)):
-        TestNum = ToTestNums[i]
-        TestDigit = ToTestDigits[i]
-        Count = 0
-        for k in range(10):
-            if int(str(TestNum).replace(str(TestDigit), str(k))) in Primes:
-                Count += 1
-        if Count == 8:
-            print(TestNum)
-            break
