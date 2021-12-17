ea2182d4df1b96ad1bbd6014171a2fc23e894466
MrMooooYT <mrmooooyt@gmail.com>
2021-12-17 15:57:50 +0000

    Euler 51 incomplete

--- live_events/project_euler/Ruoyu/euler51.py
+++ live_events/project_euler/Ruoyu/euler51.py
@@ -22,17 +22,28 @@ def IsPossibleNum(n):
     strn = str(n)
     for i in range(10):
         pvlist = [] #place value list
-        if strn.count(str(i)) % 2 == 0: #CHANGE TO 3
+        if strn.count(str(i)) > 0: #and strn.count(str(i)) % 3 == 0:
             for pv, digit in enumerate(strn):
                 if digit == str(i):
                     pvlist.append(pv)
-            pass
+            #import pdb;pdb.set_trace()
+            for a in range(len(pvlist) - 1): #CHANGE TO 2
+                for b in range(a + 1, len(pvlist)): #CHANGE TO 1 AND ADD EXTRA LOOP
+                    Tempa = pvlist[a]
+                    Tempb = pvlist[b]
+                    PossibleNums.append(strn[:Tempa] + strn[Tempa + 1:Tempb] + strn[Tempb + 1:] + ',' + str(Tempa) + str(Tempb))
+
 
 #Test
-while True:
-    CurrentPow += 1
-    PossibleNums = []
-    for i in range(10 ** (CurrentPow - 1), 10 ** CurrentPow + 1):
-        if IsPrime(i):
-            Primes.append(i)
-            print(i)
+if __name__ == '__main__':
+    while True:
+        CurrentPow += 1
+        PossibleNums = []
+        for i in range(10 ** (CurrentPow - 1), 10 ** CurrentPow + 1):
+            if IsPrime(i):
+                Primes.append(i)
+                print(i)
+        for i in PossibleNums:
+            if PossibleNums.count(i) >= 7:
+                print(i)
+                break
---
+++ live_events/project_euler/Ruoyu/test.py
@@ -0,0 +1,4 @@
+import euler51 as euler
+
+euler.IsPossibleNum(56333)
+print(euler.PossibleNums)
