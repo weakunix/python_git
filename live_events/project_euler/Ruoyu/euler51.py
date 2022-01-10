309b1383559effd45809dac72748f4e8dd5a1ed5
R. Xu <ruoyu.xu2008@icloud.com>
2022-01-10 09:06:14 +0800

    Euler 51 incomplete

--- live_events/project_euler/Ruoyu/euler51.py
+++ live_events/project_euler/Ruoyu/euler51.py
@@ -3,11 +3,11 @@ import numpy as np
 
 #Vars
 Primes = []
-PossibleNums = []
+ValueCount = {}
 CurrentPow = 0
 
 #User def functions
-def IsPrime(n):
+def IsPrime(n) -> bool:
     if n <= 1:
         return False
     MaxTest = int(np.sqrt(n))
@@ -18,36 +18,41 @@ def IsPrime(n):
             return False
     return True
 
-def IsPossibleNum(n):
-    strn = str(n)
-    for i in range(10):
-        pvlist = [] #place value list
-        if strn.count(str(i)) > 0 and strn.count(str(i)) % 3 == 0:
-            for pv, digit in enumerate(strn):
-                if digit == str(i):
-                    pvlist.append(pv)
-            #import pdb;pdb.set_trace()
-            for a in range(len(pvlist) - 2): 
-                for b in range(a + 1, len(pvlist) - 1): 
-                    for c in range(b + 1, len(pvlist)): 
-                        Tempa = pvlist[a]
-                        Tempb = pvlist[b]
-                        Tempc = pvlist[c]
-                        PossibleNums.append(strn[:Tempa] + strn[Tempa + 1:Tempb] + strn[Tempb + 1:Tempc] + strn[Tempc + 1:] + ',' + str(Tempa) + str(Tempb) + str(Tempc))
+def ListCombinations(l, ChooseNum) -> list:
+    if ChooseNum == 0:
+        return [[]]
+    if ChooseNum == len(l):
+        return [l]
+    x = ListCombinations(l[1:], ChooseNum - 1)
+    for i in range(len(x)):
+        x[i] = [l[0]] + x[i]
+    y = ListCombinations(l[1:], ChooseNum)
+    return x + y
 
+def IsPossibleNum(n):
+    global ValueCount
+    strn = str(n)
+    listn = [i for i in strn]
+    DigitCount = [[] for _ in range(10)]
+    for i, k in enumerate(strn):
+        k = int(k)
+        DigitCount[k].append(i)
+    for i, k in enumerate(DigitCount):
+        if len(k) >= 3:
+            Combinations = ListCombinations(k, 3)
+            for c in Combinations:
+                temp = listn
+                for d in c:
+                    temp[d] = 'x'
 
 #Test
 if __name__ == '__main__':
     while True:
         CurrentPow += 1
-        PossibleNums = []
+        ValueCount = {}
         for i in range(10 ** (CurrentPow - 1), 10 ** CurrentPow + 1):
             if IsPrime(i):
                 Primes.append(i)
                 IsPossibleNum(i)
                 print(i)
         #import pdb;pdb.set_trace()
-        for i in set(PossibleNums):
-            if PossibleNums.count(i) >= 8:
-                print(i)
-                raise SystemExit()
