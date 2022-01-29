e2be575a7ec601f2462cc19bb5c4f22103fb3905
R. Xu <ruoyu.xu2008@icloud.com>
2022-01-30 06:07:20 +0800

    Euler 60 complete

--- live_events/project_euler/Ruoyu/euler60.py
+++ live_events/project_euler/Ruoyu/euler60.py
@@ -4,100 +4,127 @@ Sum must be odd (naturally occurs)
 All set members must be same mod 3 unless 3 is in the set
 
 Approach:
-BFI: try primes up to certain amount (first up to 1000)
 Get primes from prime file
 Sort primes by mod 3 result
 Make list into dictionary
 Test sets
 '''
 
+#Imports
+import numpy as np
+
 #Vars
-FailValues = [2, 5] #Primes that won't work
-Primes = set() #All primes
-Mod1Primes = [] #Primes that are 1 mod 3 + 3 itself
-Mod2Primes = [] #Primes that are 2 mode 3 + 3 itself
-PrimeDict = {} #Dictionary of prime pairs
-Pairs = [] #Pairs of primes
-Trips = [] #Triples of primes
-Quads = [] #Quadruples of primes
-IsValidAddition = False #Is new prime valid addition to set
+Count = 1 #Count
+EarlyPrimes = set() #Primes added early
+EarlyNotPrimes = set() #Confirmed not primes
+PrimeList = [] #All primes list
+Mod1PrimesDict = {} #Dictionary of mod 3 = 1 prime pairs
+Mod2PrimesDict = {} #Dictionary of mod 3 = 2 prime pairs
+Mod1Trips = [] #Triples of mod 3 = 1 primes
+Mod2Trips = [] #Triplets of mod 3 = 2 primes
+Mod1Quads = [] #Quadruples of 3 = 1 primes
+Mod2Quads = [] #Quadruples of 3 = 2 primes
+IsValidAddition = False #Is new prime a valid addition to set
 
 #User def functions
-def ListCombinations(l, ChooseNum) -> list:
-    if ChooseNum == 0:
-        return [[]]
-    if ChooseNum == len(l):
-        return [l]
-    x = ListCombinations(l[1:], ChooseNum - 1)
-    for i in range(len(x)):
-        x[i] = [l[0]] + x[i]
-    y = ListCombinations(l[1:], ChooseNum)
-    return x + y
+def IsPrime(n, IsEarly = False) -> bool:
+    if n in EarlyPrimes:
+        if not IsEarly:
+            PrimeList.append(n)
+            EarlyPrimes.remove(n)
+        return True
+    if n in EarlyNotPrimes:
+        if not IsEarly:
+            EarlyNotPrimes.remove(n)
+        return False
+    MaxTestVal = int(np.sqrt(n))
+    IsPrimeVar = True
+    FinishTesting = False
+    for p in PrimeList:
+        if p > MaxTestVal:
+            FinishTesting = True
+            break
+        if n % p == 0:
+            IsPrimeVar = False
+            break
+    if not FinishTesting and n > 2:
+        for i in range(PrimeList[-1] + 1, MaxTestVal + 1):
+            if IsPrime(i, True):
+                if n % i == 0:
+                    IsPrimeVar = False
+    if IsPrimeVar:
+        if IsEarly:
+            EarlyPrimes.add(n)
+        else:
+            PrimeList.append(n)
+        return True
+    if IsEarly:
+        EarlyNotPrimes.add(n)
+    return False
+
+def SortPrime(p) -> int:
+    FailValues = [2, 5]
+    if p in [2, 5]:
+        return 0
+    if p == 3:
+        Mod1PrimesDict[3] = []
+        Mod2PrimesDict[3] = []
+        return 0
+    if p % 3 == 1:
+        return 1
+    return 2
+
+
 
 def IsValidPair(a, b) -> bool:
-    return int(a + b) in Primes and int(b + a) in Primes
+    a = str(a)
+    b = str(b)
+    return IsPrime(int(a + b), True) and IsPrime(int(b + a), True)
 
-def ConvertToDict(l) -> None:
-    for Pair in l:
-        a = Pair[0]
-        b = Pair[1]
-        if IsValidPair(str(a), str(b)):
-            try:
-                PrimeDict.get(a).add(b)
-            except:
-                PrimeDict[a] = {b}
-
-def AddMembers(l, IsFinalMember = False):
-    NewSets = []
-    MinSum = None
-    for p in l:
-        try:
-            for Prime in PrimeDict[p[-1]]:
-                IsValidAddition = True
-                for i in p[:-1]:
-                    if Prime not in PrimeDict[i]:
-                        IsValidAddition = False
-                        break
-                if IsValidAddition:
-                    if IsFinalMember:
-                        try:
-                            if sum(p) + Prime < MinSum:
-                                MinSum = sum(p) + Prime
-                        except:
-                            MinSum = sum(p) + Prime
-                    else:
-                        NewSets.append(p + [Prime])
-        except:
-            pass
-    if IsFinalMember:
-        return MinSum
-    return NewSets
-
-#Get primes
-with open('million_primes.txt', 'r') as PrimeFile:
-    for i in PrimeFile:
-        i = int(i)
-        Primes.add(i)
-        if i > 10 ** 4:
-            pass
-        elif i == 3:
-            Mod1Primes.append(i)
-            Mod2Primes.append(i)
-        elif i % 3 == 1:
-            Mod1Primes.append(i)
-        else:
-            Mod2Primes.append(i)
-print('Primes obtained')
 
 #Main
 if __name__ == '__main__':
-    Pairs = ListCombinations(Mod1Primes, 2) #First param is the prime list to test
-    ConvertToDict(Pairs)
-    print(f'Pairs done with {len(Pairs)} elements')
-    Trips = AddMembers(Pairs)
-    print(f'Trips done with {len(Trips)} elements')
-    Quads = AddMembers(Trips, True)
-    print(Quads)
-    #print(f'Quads done with {len(Quads)} elements')
-    #Quints = AddMembers(Quads, True)
-    #print(Quints)
+    while True:
+        Count += 1
+        if Count % 100 == 0:
+            print(Count)
+        if IsPrime(Count):
+            temp = SortPrime(Count)
+            if temp:
+                if temp == 1:
+                    temp = Mod1PrimesDict
+                    trips_temp = Mod1Trips
+                    quads_temp = Mod1Quads
+                else:
+                    temp = Mod2PrimesDict
+                    trips_temp = Mod2Trips
+                    quads_temp = Mod2Quads
+                for i in temp:
+                    if IsValidPair(Count, i):
+                        temp.get(i).append(Count)
+                for q in quads_temp:
+                    IsValidAddition = True
+                    for p in q:
+                        if Count not in temp[p]:
+                            IsValidAddition = False
+                            break
+                    if IsValidAddition:
+                        print(q + [Count], sum(q) + Count)
+                        raise SystemExit()
+                for t in trips_temp:
+                    IsValidAddition = True
+                    for p in t:
+                        if Count not in temp[p]:
+                            IsValidAddition = False
+                            break
+                    if IsValidAddition:
+                        quads_temp.append(t + [Count])
+                for p in temp: #Could store these in a list previously to make it faster
+                    if Count in temp[p]:
+                        for Element in temp[p]:
+                            try:
+                                if Count in temp[Element]:
+                                    trips_temp.append([p, Element, Count])
+                            except:
+                                pass
+                temp[Count] = []
