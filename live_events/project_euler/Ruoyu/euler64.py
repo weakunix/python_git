566932330d9f976d9693f4f9a8e10a34457bd725
MrMooooYT <mrmooooyt@gmail.com>
2022-01-31 18:56:23 +0000

    Euler 64 incomplete

--- live_events/project_euler/Ruoyu/euler64.py
+++ live_events/project_euler/Ruoyu/euler64.py
@@ -39,11 +39,30 @@ Count = 0 #Count
 PerfectSquares = {i ** 2 for i in range(1, 100)} #Perfect squares
 FirstCase = 0 #First period case
 
+#User def functions
+def NextPeriod(Current, i) -> tuple:
+    p = Current[0]
+    q = Current[1]
+    d = Current[2]
+    f = int((p * np.sqrt(i) + q) / d)
+    return (d * p, d * (d * f - q), p ** 2 * i - (q - d * f) ** 2)
+
 #Main
 if __name__ == '__main__':
-    for i in range(2, 10 ** 4 + 1):
+    #for i in range(2, 14): #10 ** 4 + 1):
+    for i in range(23, 24):
+        import pdb;pdb.set_trace()
         if i not in PerfectSquares:
             Count = 1
             temp = np.sqrt(i)
             a0 = int(temp)
-            
+            temp = (i - a0 ** 2, a0 ** 3 - i * a0, i - a0 ** 2)
+            FirstCase = temp
+            while True:
+                temp = NextPeriod(temp, i)
+                Count += 1
+                if temp == FirstCase:
+                    break
+            if Count % 2 == 0: #Count has the first element of period twice thus all odd periods have even "Count"
+                TotalOddPeriods += 1
+    print(TotalOddPeriods)
