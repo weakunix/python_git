53543ec089fd4bd246fa85e56663f374729de89d
Ruoyu Xu <ruoyu.xu2008@icloud.com>
2022-02-01 11:39:13 -0500

    Euler 64 complete

--- live_events/project_euler/Ruoyu/euler64.py
+++ live_events/project_euler/Ruoyu/euler64.py
@@ -36,27 +36,39 @@ import numpy as np
 #Vars
 TotalOddPeriods = 0 #TotalOddPeriods
 Count = 0 #Count
-PerfectSquares = {i ** 2 for i in range(1, 100)} #Perfect squares
+PerfectSquares = {i ** 2 for i in range(1, 101)} #Perfect squares
 FirstCase = 0 #First period case
 
 #User def functions
+def Gcd(a, b) -> int:
+    while a != 0 and b != 0:
+        a %= b
+        if a == 0:
+            return b
+        b %= a
+        if b == 0:
+            return a
+
+def SimplifyFrac(t) -> tuple:
+    g = Gcd(Gcd(t[0], t[1]), t[2])
+    return (t[0] // g, t[1] // g, t[2] // g)
+
 def NextPeriod(Current, i) -> tuple:
     p = Current[0]
     q = Current[1]
     d = Current[2]
     f = int((p * np.sqrt(i) + q) / d)
-    return (d * p, d * (d * f - q), p ** 2 * i - (q - d * f) ** 2)
+    t = (d * p, d * (d * f - q), p ** 2 * i - (q - d * f) ** 2)
+    return SimplifyFrac(t)
 
 #Main
 if __name__ == '__main__':
-    #for i in range(2, 14): #10 ** 4 + 1):
-    for i in range(23, 24):
-        import pdb;pdb.set_trace()
+    for i in range(2, 10 ** 4 + 1):
         if i not in PerfectSquares:
             Count = 1
             temp = np.sqrt(i)
             a0 = int(temp)
-            temp = (i - a0 ** 2, a0 ** 3 - i * a0, i - a0 ** 2)
+            temp = (1, a0, i - a0 ** 2)
             FirstCase = temp
             while True:
                 temp = NextPeriod(temp, i)
@@ -64,5 +76,6 @@ if __name__ == '__main__':
                 if temp == FirstCase:
                     break
             if Count % 2 == 0: #Count has the first element of period twice thus all odd periods have even "Count"
+                print(i)
                 TotalOddPeriods += 1
     print(TotalOddPeriods)
