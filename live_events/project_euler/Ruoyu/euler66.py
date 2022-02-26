3de3c911d6aa4412f5e86bd4d721cadabd44587c
R. Xu <ruoyu.xu2008@icloud.com>
2022-02-27 07:15:46 +0800

    Euler 66 complete

--- live_events/project_euler/Ruoyu/euler66.py
+++ live_events/project_euler/Ruoyu/euler66.py
@@ -1,57 +1,56 @@
 '''
 Observations:
-We can factorize x^2 - Dy^2 = (x - √D * y)(x + √D * y) = 1
+x^2 - 1 = Dy^2
+(x + 1)(x - 1) = Dy^2
 
-Possibilities:
-Try BFI through all D <= 1000 and find minimal solutions
-
-Or
-
-Instead of looping through D, we loop through x until there is only one D left between 1 to 100
-For every x >= 2, we check every y^2 and find every (x^2 - 1) / y^2
-If that is an integer, we remove that D from the list
-Optimization: instead of checking every y^2, run some sort of prime factorization algorithm to figure out which y^2 will work
+Approach:
+Chakravala method (google for reminder)
 '''
 
-#Imports
-import numpy as np
-
 #Vars
-LargestX = 0 #Largest x
-DVal = 0 #Corresponding D value for largest x
-PerfectSquares = set(i ** 2 for i in range(1, 33)) #Set of perfect squares
-NotSquares = set() #Set of non perfect squares
+DForMax = (None, None) #Value of D for maximum x in the form of (x, D)
+Squares = {i ** 2 for i in range(1, 32)}
 
 #User def functions
-def IsPerfectSquare(n) -> bool:
-    if n in PerfectSquares:
-        return True
-    if n in NotSquares:
-        return False
-    if int(np.sqrt(n)) == np.sqrt(n):
-        PerfectSquares.add(n)
-        return True
-    NotSquares.add(n)
-    return False
-
-def GetMinX(D) -> int:
-    x = 1
+def GetMinM(trip, D) -> int:
+    m = 0
+    x = trip[0]
+    y = trip[1]
+    k = trip[2]
+    #k | x + ym
     while True:
-        x += 1
-        temp = x ** 2 - 1
-        if temp % D == 0:
-            temp //= D
-            if IsPerfectSquare(temp):
-                return x
+        m += 1
+        if (x + y * m) % k == 0 and m ** 2 > D:
+            return m
+
+def ComposeTriples(a, b, D) -> tuple:
+    x = a[0]
+    y = a[1]
+    k = a[2]
+    m = b[0]
+    k2 = b[2]
+    NewX = (x * m + y * D) // k
+    NewY = (x + y * m) // k
+    NewK = k2 // k
+    return (NewX, NewY, NewK)
 
 #Main
 if __name__ == '__main__':
-    for D in range(2, 1001):
-        print(D)
-        if D not in PerfectSquares:
-            NotSquares.add(D)
-            temp = GetMinX(D)
-            if temp > LargestX:
-                LargestX = temp
-                DVal = D
-    print(LargestX, DVal)
+    for D in range(1, 1001):
+        if D not in Squares:
+            print(D)
+            x = 0
+            while x ** 2 < D:
+                x += 1
+            y = 1
+            k = x ** 2 - D
+            temp = (x, y, k)
+            while temp[2] != 1:
+                m = GetMinM(temp, D)
+                temp = ComposeTriples(temp, (m, 1, m ** 2 - D), D)
+            try:
+                if temp[0] > DForMax[0]:
+                    DForMax = (temp[0], D)
+            except:
+                DForMax = (temp[0], D)
+    print(DForMax)
