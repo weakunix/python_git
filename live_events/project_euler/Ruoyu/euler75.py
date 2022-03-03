625aedc1cebb88b38848ec9b11272166a51b1d8e
R. Xu <ruoyu.xu2008@icloud.com>
2022-03-04 02:35:36 +0800

    Euler 75 complete

--- live_events/project_euler/Ruoyu/euler75.py
+++ live_events/project_euler/Ruoyu/euler75.py
@@ -1,45 +1,81 @@
 '''
 Observations:
-(a) If n is odd, the pythagorean triple is (n, (n^2 - 1) / 2, (n^2 + 1) / 2)
-(b) If n is even, the pythagorean triple is (n, (n / 2)^2 - 1, (n / 2)^2 + 1)
+(a) A mutually prime pythagorean triple can be written as a = m^2 - n^2, b = 2mn, c = m^2 + n^2 where m and n are mutually prime and exactly one of m or n is even
+(b) b must be divisible by 4
+
+Approach:
+Generate all possible pythagorean triples using Observation (a) and Observation (b)
+Then loop through them and find unique perimeters
 '''
+
+#Imports
+import math as albreto
 
 #Vars
 PythagTrips = set() #Set of pythagorean triples that sum to less than 1.5 million
-RightTriPerimeters = {} #Dict of n: how many pythagorean triples sum to n
+RightTriPerims = {} #Dict of n: how many pythagorean triples sum to n
 Total = 0 #Total perimeters unique to one right triangle
 
 #User def functions
-def GetPythagTrip(n) -> tuple:
-    if n % 2 == 0:
-        temp = [n, (n // 2) ** 2 - 1, (n // 2) ** 2 + 1]
-    else:
-        temp = [n, (n ** 2 - 1) // 2, (n ** 2 + 1) // 2]
-    temp.sort()
-    return tuple(temp)
+def Gcd(a, b) -> int:
+    while True:
+        if a % b == 0:
+            return b
+        a %= b
+        if b % a == 0:
+            return a
+        b %= a
+
+def GetMutuallyPrimePairs(n) -> list:
+    Pairs = []
+    max_test_val = int(albreto.sqrt(n)) + 1
+    for i in range(1, max_test_val):
+        if n % i == 0:
+            if Gcd(i, n // i) == 1:
+                Pairs.append((i, n // i))
+                Pairs.append((n // i, i))
+    return Pairs
+
+def GetPythagTrips(b) -> list:
+    b //= 2
+    m = 1
+    Trips = set()
+    while b % 2 == 0:
+        m *= 2
+        b //= 2
+    MutuallyPrimePairs = GetMutuallyPrimePairs(b)
+    OriginalM = m
+    for p in MutuallyPrimePairs:
+        m = OriginalM * p[0]
+        n = p[1]
+        if m > n:
+            temp = [m ** 2 - n ** 2, 2 * m * n, m ** 2 + n ** 2]
+        else:
+            temp = [n ** 2 - m ** 2, 2 * m * n, m ** 2 + n ** 2]
+        temp.sort()
+        Trips.add(tuple(temp))
+    return Trips
 
 #Main
 if __name__ == '__main__':
-    for n in range(3, 5):
-        while True:
-            temp = GetPythagTrip(n)
-            if sum(temp) > 120: #15 * 10 ** 5:
-                break
+    b = 4
+    while b <= 1.5 * 10 ** 6:
+        if b % 1000 == 0:
+            print(b)
+        temp = GetPythagTrips(b)
+        for p in temp:
             count = 1
-            while True:
-                temp_2 = (temp[0] * count, temp[1] * count, temp[2] * count)
-                if sum(temp_2) > 120: #15 * 10 ** 5:
-                    break
-                PythagTrips.add(temp_2)
+            while count * sum(p) <= 1.5 * 10 ** 6:
+                PythagTrips.add((p[0] * count, p[1] * count, p[2] * count))
                 count += 1
-            n += 2
+        b += 4
     for t in PythagTrips:
         t = sum(t)
         try:
-            RightTriPerimeters[t] += 1
+            RightTriPerims[t] += 1
         except:
-            RightTriPerimeters[t] = 1
-    for p in RightTriPerimeters:
-        if RightTriPerimeters[p] == 1:
+            RightTriPerims[t] = 1
+    for p in RightTriPerims:
+        if RightTriPerims[p] == 1:
             Total += 1
     print(Total)
