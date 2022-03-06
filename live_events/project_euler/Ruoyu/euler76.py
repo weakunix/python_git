08470e2f0601a9a494b81b39697c68468b7969a9
R. Xu <ruoyu.xu2008@icloud.com>
2022-03-07 04:14:56 +0800

    Euler 76 revised Euler 77 complete

--- live_events/project_euler/Ruoyu/euler76.py
+++ live_events/project_euler/Ruoyu/euler76.py
@@ -1,3 +1,4 @@
+#METHOD 1
 '''
 Observations:
 (a) Let f(n) return a tuple of the number of sums with k integers from 0 to n
@@ -21,17 +22,64 @@ We can find the succeeding f(n) by using Observation (c)
 '''
 
 #Vars
-SumDict = {0: (0, 1), 1: (0, 1), 2: (0, 1, 1)} #Dict of previous sums
+SumDict = {0: (0, 1), 1: (0, 1), 2: (0, 1, 1)} #Dict of previous f(n)
+New = () #New f(n)
 
 #Main
-for n in range(3, 101):
-    New = [0, 1]
-    for k in range(2, n + 1):
-        count = 0
-        for a, b in enumerate(SumDict[n - k]):
-            if a > k:
-                break
-            count += b
-        New.append(count)
-    SumDict[n] = tuple(New)
-print(sum(New) - 1)
+if __name__ == '__main__':
+    for n in range(3, 101):
+        New = [0, 1]
+        for k in range(2, n + 1):
+            count = 0
+            for a, b in enumerate(SumDict[n - k]):
+                if a > k:
+                    break
+                count += b
+            New.append(count)
+        SumDict[n] = tuple(New)
+    print(sum(New) - 1)
+
+#METHOD 2
+'''
+Method 1 is great but it doesn't work for Euler 77
+Thus Method 2 has been created:
+
+Observations:
+(a) Let f(n) return a dictionary of the number of sums using k and numbers >= k for 1 <= k <= n
+For example, f(5) would be {1: 5, 2: 1, 3: 0, 4: 0, 5: 1}
+Using numbers >= 1, there are 5 combinations: (1, 4), (1, 1, 3), (1, 1, 1, 2), (1, 1, 1, 1), (1, 2, 2)
+Using numbers >= 2, there is 1 combination: (2, 3) since we cannot use 1
+Using numbers >= 3 and 4, there are 0 combinations since we cannot use 1 or 2
+Using numbers >= 5, there is 1 combination: (5)
+(b) We define f(n) in such a way because it allows us to find f(n) using f(n - k)
+(c) f(0) returns 1 no matter what since this is only the case when we try to write n as the sum of 1 number
+(d) f(1) = {1: 1}, f(2) = {1: 1, 2: 1}
+(e) To get f(n), we will loop through k where 1 <= k <= n and take the sum of f(n - k)'s elements with keys >= k to avoid multiple counting
+(f) Observation (e) guarentees that the order of numbers will be increasing, thus each sum is only counted once
+
+Approach:
+Store all previous f(n) (defined in Observation (a)) and loop through all n where 3 <= n <= 100
+Use Observation (e) to get each succeeding f(n)
+'''
+
+#Vars
+SumDict = {1: {1: 1}, 2: {1: 1, 2: 1}} #Dict of previous f(n)
+Total = 0 #Total sums of 100
+
+#Main
+if __name__ == '__main__':
+    for n in range(3, 101):
+        New = {}
+        for i in range(1, n + 1):
+            count = 0
+            try:
+                temp = SumDict[n - i]
+                for k in range(i, len(temp) + 1):
+                    count += temp[k]
+            except:
+                count = 1
+            New[i] = count
+        SumDict[n] = New
+    for i in New:
+        Total += New[i]
+    print(Total - 1)
---
+++ live_events/project_euler/Ruoyu/euler77.py
@@ -0,0 +1,59 @@
+'''
+Observations:
+(a) Let f(n) return a dictionary of the number of sums using k and prime numbers >= k for 2 <= k <= n
+For example, f(10) would be {2: 3, 3: 1, 5: 1, 7: 0}
+Using prime numbers >= 2, there are 3 combinations: (2, 2, 2, 2, 2), (2, 2, 3, 3), (2, 3, 5) 
+Using prime numbers >= 3, there is 1 combination: (3, 7) since we cannot use 2
+Using prime numbers >= 5, there is 1 combination: (5, 5) since we cannot use 2 or 3
+Using prime numbers >= 7, there are 0 combinations since we cannot use 2,  or 5
+(b) We define f(n) in such a way because it allows us to find f(n) using f(n - k)
+(c) f(0) returns 1 no matter what since this is only the case when we try to write n as the sum of 1 prime number and we will only consider f(0) when n is prime
+(d) f(1) = {}, f(2) = {2: 1}
+(e) To get f(n), we will loop through primes k where 2 <= k <= n and take the sum of f(n - k)'s elements with keys >= k to avoid multiple counting
+(f) Observation (e) guarentees that the order of numbers will be increasing, thus each sum is only counted once
+
+Approach:
+Store all previous f(n) (defined in Observation (a)) and loop through all n until an n that has a total of more than 5000 prime sums is found 
+Use Observation (e) to get each succeeding f(n)
+'''
+
+#Imports
+import math as albreto
+
+#Vars
+SumDict = {1: {}, 2: {2: 1}} #Dict of previous f(n)
+Primes = [2] #List of primes
+Total = 0 #Total prime sums of n
+n = 2 #n
+
+#User def functions
+def IsPrime(n) -> bool:
+    mtv = int(albreto.sqrt(n))
+    for p in Primes:
+        if p > mtv:
+            Primes.append(n)
+            return True
+        if n % p == 0:
+            return False
+
+#Main
+if __name__ == '__main__':
+    while Total <= 5000:
+        n += 1
+        New = {}
+        IsPrime(n)
+        for i in Primes:
+            count = 0
+            try:
+                temp = SumDict[n - i]
+                for k in temp:
+                    if i <= k:
+                        count += temp[k]
+            except:
+                count = 1
+            New[i] = count
+        SumDict[n] = New
+        Total = 0
+        for i in New:
+            Total += New[i]
+    print(n)
