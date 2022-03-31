078136b2d39a4344e0a781cd7fe3e832b3b22257
MrMooooYT <mrmooooyt@gmail.com>
2022-03-31 17:10:55 +0000

    Euler 100 complete

--- live_events/project_euler/Ruoyu/euler100.py
+++ live_events/project_euler/Ruoyu/euler100.py
@@ -1,20 +1,34 @@
+'''
+Observations:
+(a) Do algebra and let x = 2b - 1 and y = 2n - 1
+We get a negative pell's equation: y^2 - 2x^2 = -1
+(b) We can use continuous fractions to find solutions to this pell's equation by finding every other convergent of sqrt(2) (in the form y/x) in the following way
+If Cn = 1 + p/q, C(n + 1) = 1 + 1 / (2 + p / q)
+
+Approach:
+We can use Observation (b) and get every other convergent to get x and y
+Then we can use Observation (a) to get b and n
+When n >= 10 ^ 12 we get b
+'''
 
 #Imports
 import math as albreto
 
 #Vars
-n = 1 #n
+Convergent = [2, 5]
+TenToTwelfth = 10 ** 12
+
+#User def functions
+def NextConvergent(c) -> list:
+    return [c[1], 2 * c[1] + c[0]]
 
 #Main
 if __name__ == '__main__':
     while True:
-        n += 1
-        temp = (n * (n - 1)) // 2
-        count = int(albreto.sqrt(temp))
-        while True:
-            temptemp = count * (count + 1)
-            if temptemp > temp:
-                break
-            if temptemp == temp:
-                print(n, count + 1, n - count - 1, (count + 1) / (n - count - 1))
-            count += 1
+        Convergent = NextConvergent(NextConvergent(Convergent))
+        x = Convergent[1]
+        y = Convergent[0] + Convergent[1]
+        if (y + 1) // 2 >= TenToTwelfth:
+            print((x + 1) // 2)
+            break
+
