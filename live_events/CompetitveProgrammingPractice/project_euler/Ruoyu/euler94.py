0b12caf5f1242a59dab3077374b9dd0444e30d0d
MrMooooYT <mrmooooyt@gmail.com>
2022-04-12 18:37:48 +0000

    Euler 94 incomplete

--- live_events/CompetitveProgrammingPractice/project_euler/Ruoyu/euler94.py
+++ live_events/CompetitveProgrammingPractice/project_euler/Ruoyu/euler94.py
@@ -5,16 +5,19 @@ import math as albreto
 #Vars
 Total = 0 #Sum of perimeters of all equilateral triangles with integral side lengths and area
 OneBil = 10 ** 9 #1 billion (const)
+m = 1 #m
+StillConsiderSecondCase = True #Is second case perimeter still in range (4m^2 <= 1 billion)
 
 #Main
 if __name__ == '__main__':
-    for i in range(2, 333333334):
-        if i % 10000 == 0:
-            print(i)
-        if albreto.sqrt((3 * i + 1) * (i - 1)) % 1 == 0:
-            print('FOUND ONE GET TROLLED BRRRRRRRR')
-            Total += 3 * i + 1
-        if albreto.sqrt((3 * i - 1) * (i + 1)) % 1 == 0:
-            print('FOUND ONE GET TROLLED BRRRRRRRR')
-            Total += 3 * i - 1
-    print(Total)
+    while True:
+        m += 1
+        if StillConsiderSecondCase:
+            p = 4 * m ** 2
+            if p > OneBil:
+                StillConsiderSecondCase = False
+            elif m % 3 != 0:
+                temp = (m ** 2 - 1) // 3
+                if albreto.sqrt(temp) % 1 == 0:
+                    Total += p
+
