e3105b2af2d05967342264726bb88362efd0feb2
R. Xu <ruoyu.xu2008@icloud.com>
2022-05-05 04:27:56 +0800

    Out sortsg

---
+++ live_events/CompetitveProgrammingPractice/USACO/2017-2018/USOpen/Silver/AfterContest/OutSorts/OutSorts.cpp
@@ -0,0 +1,36 @@
+#include <fstream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+#define all(v) v.begin(), v.end()
+
+ofstream fout("sort.out");
+ifstream fin("sort.in");
+
+int main() {
+    int N, MinPasses = 0, ToRight = 0;
+    unordered_map<int, vector<int> > FinalIndexes;
+    fin >> N;
+    vector<int> Arr(N), SortedArr;
+    for (int i = 0; i < N; i++) fin >> Arr[i];
+    SortedArr = Arr;
+    sort(all(SortedArr));
+    for (int i = 0; i < N; i++) {
+        if (FinalIndexes.count(SortedArr[i]) == 0) FinalIndexes[SortedArr[i]] = {i};
+        else FinalIndexes[SortedArr[i]].push_back(i);
+    }
+    for (int i = 0; i < N; i++) {
+        int a = FinalIndexes[Arr[i]][0];
+        FinalIndexes[Arr[i]].erase(FinalIndexes[Arr[i]].begin());
+        Arr[i] = a;
+    }
+    for (int i = 0; i < N; i++) {
+        if (Arr[i] > i) ToRight++;
+        else MinPasses = max(i - Arr[i], MinPasses);
+    }
+    fout << max(MinPasses, ToRight) + 1 << "\n";
+    return 0;
+}
