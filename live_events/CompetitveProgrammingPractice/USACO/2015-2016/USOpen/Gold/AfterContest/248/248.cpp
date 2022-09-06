8e6046c5a7f3a69d4673a0d1e6db8f0a232243e6
MrMooooYT <mrmooooyt@gmail.com>
2022-09-05 22:04:28 -0400

    248

---
+++ live_events/CompetitveProgrammingPractice/USACO/2015-2016/USOpen/Gold/AfterContest/248/248.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N, Ans = 0;
+vector<int> Nums;
+
+int main() {
+    freopen("248.in", "r", stdin);
+    freopen("248.out", "w", stdout);
+
+    cin >> N;
+    Nums.resize(N);
+    for (int i = 0; i < N; i++) cin >> Nums[i];
+
+    vector<vector<int> > Dp(N, vector<int>(N, -1));
+
+    for (int i = 0; i < N; i++) {
+        Dp[i][i] = Nums[i];
+        Ans = max(Nums[i], Ans);
+    }
+
+    for (int i = 1; i < N; i++) {
+        for (int j = 0; j < N - i; j++) {
+            for (int k = j; k < j + i; k++) {
+                if (Dp[j][k] != -1 and Dp[j][k] == Dp[k + 1][j + i]) {
+                    Dp[j][j + i] = Dp[j][k] + 1;
+                    Ans = max(Dp[j][j + i], Ans);
+                }
+            }
+        }
+    }
+
+    cout << Ans << "\n";
+
+    return 0;
+}
