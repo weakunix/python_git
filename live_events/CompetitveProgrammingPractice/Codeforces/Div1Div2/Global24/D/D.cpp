4e3e95a73b1ab389b7a5ec7aea38dde70cedad00
MrMooooYT <mrmooooyt@gmail.com>
2022-11-26 12:11:36 -0500

    Codeforces

---
+++ live_events/CompetitveProgrammingPractice/Codeforces/Div1Div2/Global24/D/D.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <cassert>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> simps;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+int n, p;
+
+int main() {
+    cin >> n >> p;
+	return 0;
+}
